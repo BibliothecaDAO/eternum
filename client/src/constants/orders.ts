a3e69885e57ea509f71ffc7a81f552484953f615
aymericdelab <aymericdelabrousse@gmail.com>
2023-07-01 11:44:04 +0200

    feat: get order and realm name

---
+++ client/src/constants/orders.ts
@@ -0,0 +1,18 @@
+  export const orderNameDict: { [key: number]: string } = {
+    1: "power",
+    2: "giants",
+    3: "titans",
+    4: "skill",
+    5: "perfection",
+    6: "brilliance",
+    7: "enlightenment",
+    8: "protection",
+    9: "anger",
+    10: "rage",
+    11: "fury",
+    12: "vitriol",
+    13: "fox",
+    14: "detection",
+    15: "reflection",
+    16: "twins"
+};
