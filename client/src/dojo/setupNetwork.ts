c0510e421cca6296820ef97523c770e915a5f21a
aymericdelab <aymericdelabrousse@gmail.com>
2023-07-13 10:57:36 +0200

    added torii url + bug fix

--- client/src/dojo/setupNetwork.ts
+++ client/src/dojo/setupNetwork.ts
@@ -9,6 +9,7 @@ export const KATANA_ACCOUNT_1_ADDRESS = "0x03ee9e18edc71a6df30ac3aca2e0b02a198fb
 export const KATANA_ACCOUNT_1_PRIVATEKEY = "0x0300001800000000300000180000000000030000000000003006001800006600"
 export const WORLD_ADDRESS = "0x26065106fa319c3981618e7567480a50132f23932226a51c219ffb8e47daa84"
 export const EVENT_KEY = "0x1a2f334228cee715f1f0f54053bb6b5eac54fa336e0bc1aacf7516decb0471d"
+export const TORII_URL = "http://localhost:8080/"
 
 
 export type SetupNetworkResult = Awaited<ReturnType<typeof setupNetwork>>;
@@ -21,6 +22,8 @@ export async function setupNetwork() {
 
     const signer = new Account(provider.sequencerProvider, KATANA_ACCOUNT_1_ADDRESS, ec.getKeyPair(KATANA_ACCOUNT_1_PRIVATEKEY))
 
+    // TODO: add when torii is impl in syncWorker
+    // const syncWorker = new SyncWorker(provider, contractComponents, EVENT_KEY, TORII_URL);
     const syncWorker = new SyncWorker(provider, contractComponents, EVENT_KEY);
 
     return {
@@ -29,7 +32,7 @@ export async function setupNetwork() {
         signer,
         execute: async (system: string, call_data: number.BigNumberish[]) => provider.execute(signer, system, call_data),
         entity: async (component: string, query: Query) => provider.entity(component, query),
-        entities: async (component: string, partition: string) => provider.entities(component, partition),
+        entities: async (component: string, partition: string, length: number) => provider.entities(component, partition, lenght),
         world,
         syncWorker
     };
