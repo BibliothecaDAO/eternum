3b6c7430a55c8bfeb0a465ae45104078ac13b65d
aymericdelab <aymericdelabrousse@gmail.com>
2023-07-14 12:15:12 +0200

    feat: Create/attach caravan when accepting trades

--- client/src/components/cityview/realm/trade/AcceptOffer.tsx
+++ client/src/components/cityview/realm/trade/AcceptOffer.tsx
@@ -1,23 +1,91 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SecondaryPopup } from '../../../../elements/SecondaryPopup';
 import Button from '../../../../elements/Button';
 import { Steps } from '../../../../elements/Steps';
 import { SelectCaravanPanel } from './CreateOffer';
+import { useDojo } from '../../../../DojoContext';
+import useRealmStore from '../../../../hooks/store/useRealmStore';
+import { useComponentValue } from '@dojoengine/react';
+import { getComponentValue } from '@latticexyz/recs';
+import { Utils } from '@dojoengine/core';
+import { Realm, ResourcesOffer } from '../../../../types';
 
 type AcceptOfferPopupProps = {
     onClose: () => void;
-    onAccept: () => void;
-    selectedResourceIdsGive: number[];
-    selectedResourceIdsGet: number[];
-    selectedResourcesGiveAmounts: { [key: number]: number };
-    selectedResourcesGetAmounts: { [key: number]: number };
-    resourceWeight: number;
+    selectedTradeId: number;
 }
 
-export const AcceptOfferPopup = ({ onClose, onAccept, selectedResourceIdsGet, selectedResourceIdsGive, selectedResourcesGetAmounts, selectedResourcesGiveAmounts, resourceWeight }: AcceptOfferPopupProps) => {
+export const AcceptOfferPopup = ({ onClose, selectedTradeId }: AcceptOfferPopupProps) => {
     const [selectedCaravan, setSelectedCaravan] = useState<number>(0);
     const [isNewCaravan, setIsNewCaravan] = useState(false);
     const [donkeysCount, setDonkeysCount] = useState(0);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(false);
+    }, [selectedTradeId])
+
+    const {
+        systemCalls: { attach_caravan, take_fungible_order, create_free_transport_unit, create_caravan, change_order_status },
+        components: { Trade, Status, FungibleEntities, Resource, Realm },
+    } = useDojo();
+
+    const { realmEntityId } = useRealmStore();
+
+    let trade = getComponentValue(Trade, Utils.getEntityIdFromKeys([BigInt(selectedTradeId)]));
+
+    const acceptOffer = async () => {
+        if (isNewCaravan) {
+            setIsLoading(true);
+            const transport_units_id = await create_free_transport_unit({ realm_id: realmEntityId, quantity: donkeysCount });
+            const caravan_id = await create_caravan({ entity_ids: [transport_units_id] });
+            await attach_caravan({ realm_id: realmEntityId, trade_id: selectedTradeId, caravan_id })
+            await take_fungible_order({
+                taker_id: realmEntityId,
+                trade_id: selectedTradeId
+            })
+        } else {
+            setIsLoading(true);
+            await attach_caravan({ realm_id: realmEntityId, trade_id: selectedTradeId, caravan_id: selectedCaravan });
+            await take_fungible_order({
+                taker_id: realmEntityId,
+                trade_id: selectedTradeId
+            })
+        }
+        onClose();
+    }
+
+    // set maker order
+    let makerRealm: Realm | undefined;
+    if (trade) {
+        makerRealm = getComponentValue(Realm, Utils.getEntityIdFromKeys([BigInt(trade.maker_id)]));
+    }
+
+    const resourcesGet = trade && getResources(trade.maker_order_id);
+    const resourcesGive = trade && getResources(trade.taker_order_id);
+
+    function getResources(orderId: number): ResourcesOffer[] {
+        const resources: ResourcesOffer[] = [];
+        const fungibleEntities = getComponentValue(FungibleEntities, Utils.getEntityIdFromKeys([BigInt(orderId)]));
+        if (fungibleEntities) {
+            for (let i = 0; i < fungibleEntities.count; i++) {
+                const resource = getComponentValue(
+                    Resource,
+                    Utils.getEntityIdFromKeys([BigInt(orderId), BigInt(fungibleEntities.key), BigInt(i)])
+                );
+                if (resource) {
+                    resources.push({ amount: resource.balance, resourceId: resource.resource_type });
+                }
+            }
+        }
+        return resources;
+    }
+
+    let resourceWeight = 0;
+    for (const [resourceId, amount] of Object.entries(resourcesGive?.map(resource => resource.amount) || {})) {
+        resourceWeight += amount * 1;
+    }
 
     return (
         <SecondaryPopup>
@@ -35,16 +103,19 @@ export const AcceptOfferPopup = ({ onClose, onAccept, selectedResourceIdsGet, se
                         setIsNewCaravan={setIsNewCaravan}
                         selectedCaravan={selectedCaravan}
                         setSelectedCaravan={setSelectedCaravan}
-                        selectedResourceIdsGet={selectedResourceIdsGet}
-                        selectedResourcesGetAmounts={selectedResourcesGetAmounts}
-                        selectedResourceIdsGive={selectedResourceIdsGive}
-                        selectedResourcesGiveAmounts={selectedResourcesGiveAmounts}
+                        selectedResourceIdsGet={resourcesGet?.map(resource => resource.resourceId) || []}
+                        selectedResourcesGetAmounts={resourcesGet?.map(resource => resource.amount) || []}
+                        selectedResourceIdsGive={resourcesGive?.map(resource => resource.resourceId) || []}
+                        selectedResourcesGiveAmounts={resourcesGive?.map(resource => resource.amount) || []}
                         resourceWeight={resourceWeight}
                     />
                 </div>
                 <div className='flex justify-between m-2 text-xxs'>
                     <Button className='!px-[6px] !py-[2px] text-xxs' onClick={onClose} variant='outline'>Cancel</Button>
-                    <Button className='!px-[6px] !py-[2px] text-xxs' onClick={onAccept} variant='success'>Accept Offer</Button>
+                    <div>
+                        {!isLoading && <Button className='!px-[6px] !py-[2px] text-xxs' onClick={acceptOffer} variant='success'>Accept Offer</Button>}
+                        {isLoading && <Button isLoading={true} onClick={() => {}} variant="danger" className="ml-auto p-2 !h-4 text-xxs !rounded-md"> {} </Button>}
+                    </div>
                 </div>
             </SecondaryPopup.Body>
         </SecondaryPopup >
