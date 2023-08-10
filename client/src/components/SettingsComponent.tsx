778285ce3859dbd2e85ab638905c34e1ae16c4b8
0x1337 <za6ah9t@icloud.com>
2023-08-10 23:52:13 +0700

    feat: settings popup

--- client/src/components/SettingsComponent.tsx
+++ client/src/components/SettingsComponent.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { ReactComponent as Crown } from "../assets/icons/common/crown-circle-outline.svg";
 import { ReactComponent as Settings } from "../assets/icons/common/settings.svg";
+import { ReactComponent as CloseIcon } from "../assets/icons/common/cross.svg";
+import { SecondaryPopup } from "../elements/SecondaryPopup";
+import { Headline } from "../elements/Headline";
+import SettleRealmComponent from "./cityview/realm/SettleRealmComponent";
+import Button from "../elements/Button";
+import { Checkbox } from "../elements/Checkbox";
+import { RangeInput } from "../elements/RangeInput";
 type SettingsComponentProps = {};
 
 export const SettingsComponent = ({}: SettingsComponentProps) => {
-  const [state, setState] = useState();
+  const [showSettings, setShowSettings] = useState(false);
+  const [musicLevel, setMusicLevel] = useState(50);
+  const [effectsLevel, setEffectsLevel] = useState(50);
 
   useEffect(() => {}, []);
 
@@ -14,7 +23,56 @@ export const SettingsComponent = ({}: SettingsComponentProps) => {
       <div className="text-xs font-bold">
         DeadlyCrow<span className="text-gold">.stark</span>
       </div>
-      <Settings className="ml-[6px] fill-gold translate-y-1" />
+      <Settings
+        onClick={() => setShowSettings(!showSettings)}
+        className="ml-[6px] cursor-pointer fill-gold translate-y-1"
+      />
+      {showSettings && (
+        <SecondaryPopup className="top-1/3">
+          <SecondaryPopup.Head>
+            <div className="flex items-center">
+              <div className="mr-0.5">Settings</div>
+              <CloseIcon
+                className="w-3 h-3 cursor-pointer fill-white"
+                onClick={() => {}}
+              />
+            </div>
+          </SecondaryPopup.Head>
+          <SecondaryPopup.Body width="400px">
+            <div className="flex flex-col  space-y-2 p-3">
+              <Headline size="big">Video</Headline>
+              <div className="flex text-xs text-gray-gold space-x-2 items-center">
+                <Checkbox enabled={true} />
+                <div>Fullscreen</div>
+              </div>
+              <Headline size="big">Sound</Headline>
+              <RangeInput
+                value={musicLevel}
+                fromTitle="Mute"
+                onChange={setMusicLevel}
+                title="Music"
+              />
+              <RangeInput
+                value={effectsLevel}
+                fromTitle="Mute"
+                onChange={setEffectsLevel}
+                title="Effects"
+              />
+              <Headline size="big">Settling</Headline>
+              <div className="flex justify-center">
+                <SettleRealmComponent />
+              </div>
+              <Button
+                onClick={() => setShowSettings(false)}
+                variant="outline"
+                className="text-xxs !py-1 !px-2 mr-auto"
+              >
+                Done
+              </Button>
+            </div>
+          </SecondaryPopup.Body>
+        </SecondaryPopup>
+      )}
     </div>
   );
 };
--- client/src/elements/Button.tsx
+++ client/src/elements/Button.tsx
@@ -5,25 +5,31 @@ interface ButtonProps {
   children: React.ReactNode;
   className?: string;
   disabled?: boolean;
-  variant?: "primary" | "secondary" | "success" | "danger" | "default" | "outline";
+  variant?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "default"
+    | "outline";
   isLoading?: boolean;
 }
 
 const STYLES = {
-  baseStyle: "inline-flex transition-all duration-300 items-center justify-center p-2 text-xs font-medium rounded-md",
-  primary: "!rounded-full py-1 bg-gold hover:bg-gold/50 focus:outline-none",
-  default:
-    "text-white/90 border border-transparent shadow-sm",
-  enabledStyle:
-    "bg-black/10 hover:bg-black/30 focus:outline-none",
+  baseStyle:
+    "inline-flex transition-all duration-300 items-center justify-center p-2 text-xs font-medium rounded-md",
+  primary: "rounded-full py-1 bg-gold hover:bg-gold/50 focus:outline-none",
+  default: "text-white/90 border border-transparent shadow-sm",
+  enabledStyle: "bg-black/10 hover:bg-black/30 focus:outline-none",
   disabledStyle: "bg-gray-300 cursor-not-allowed",
-  success: "border border-brilliance !text-brilliance bg-transparent hover:bg-brilliance/10",
+  success:
+    "border border-brilliance !text-brilliance bg-transparent hover:bg-brilliance/10",
   outline: "border border-gold !text-gold bg-transparent hover:bg-gold/10",
   danger: "border border-orange !text-orange bg-transparent hover:bg-orange/10",
-  secondary: "border border-orange !text-orange bg-transparent hover:bg-orange/10",
+  secondary:
+    "border border-orange !text-orange bg-transparent hover:bg-orange/10",
   loadingStyle: "relative",
-
-}
+};
 const Button: React.FC<ButtonProps> = ({
   onClick,
   children,
@@ -36,8 +42,9 @@ const Button: React.FC<ButtonProps> = ({
     <button
       type="button"
       onClick={disabled || isLoading ? undefined : onClick}
-      className={`${STYLES.baseStyle} ${STYLES[variant]} ${disabled ? STYLES.disabledStyle : STYLES.enabledStyle
-        } ${isLoading ? STYLES.loadingStyle : ""} ${className}`}
+      className={`${STYLES.baseStyle} ${STYLES[variant]} ${
+        disabled ? STYLES.disabledStyle : STYLES.enabledStyle
+      } ${isLoading ? STYLES.loadingStyle : ""} ${className}`}
       disabled={disabled || isLoading}
     >
       {isLoading ? (
