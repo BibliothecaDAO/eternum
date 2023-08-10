a8e6716b38a113d68f8ae368132e6b97224661f0
0x1337 <za6ah9t@icloud.com>
2023-08-10 22:16:43 +0700

    fix: settings icon

--- client/src/components/SettingsComponent.tsx
+++ client/src/components/SettingsComponent.tsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { ReactComponent as Crown } from '../assets/icons/common/crown-circle-outline.svg';
-import { ReactComponent as Settings } from '../assets/icons/common/settings.svg';
-type SettingsComponentProps = {}
+import React, { useEffect, useState } from "react";
+import { ReactComponent as Crown } from "../assets/icons/common/crown-circle-outline.svg";
+import { ReactComponent as Settings } from "../assets/icons/common/settings.svg";
+type SettingsComponentProps = {};
 
-export const SettingsComponent = ({ }: SettingsComponentProps) => {
-    const [state, setState] = useState();
+export const SettingsComponent = ({}: SettingsComponentProps) => {
+  const [state, setState] = useState();
 
-    useEffect(() => { }, []);
+  useEffect(() => {}, []);
 
-    return (
-        <div className='flex items-center text-white'>
-            <Crown className='mr-[6px] fill-current' />
-            <div className='text-xs font-bold'>DeadlyCrow<span className='text-gold'>.stark</span></div>
-            <Settings className='ml-[6px] fill-gold' />
-        </div>
-    );
+  return (
+    <div className="flex items-center text-white">
+      <Crown className="mr-[6px] fill-current" />
+      <div className="text-xs font-bold">
+        DeadlyCrow<span className="text-gold">.stark</span>
+      </div>
+      <Settings className="ml-[6px] fill-gold translate-y-1" />
+    </div>
+  );
 };
