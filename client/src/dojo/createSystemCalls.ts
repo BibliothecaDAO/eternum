3cf5102f7f387633a7e32f1a2aed5cc9344500fd
aymericdelab <aymericdelabrousse@gmail.com>
2023-08-16 13:35:55 +0200

    feat: set component in all system calls

--- client/src/dojo/createSystemCalls.ts
+++ client/src/dojo/createSystemCalls.ts
@@ -12,18 +12,24 @@ export function createSystemCalls(
     // TODO: this is entity id not realm id 
     const build_labor = async ({realm_id, resource_type, labor_units, multiplier}: {realm_id: number.BigNumberish, resource_type: number.BigNumberish, labor_units: number.BigNumberish, multiplier: number.BigNumberish}) => {
         const tx = await execute("BuildLabor", [realm_id, resource_type, labor_units, multiplier]);
-        await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const receipt = await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const events = getEvents(receipt);
+        setComponentsFromEvents(contractComponents, events);
     }
 
     // TODO: this is entity id not realm id 
     const harvest_labor = async ({realm_id, resource_type}: {realm_id: number.BigNumberish, resource_type: number.BigNumberish}) => {
         const tx = await execute("HarvestLabor", [realm_id, resource_type]);
-        await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const receipt = await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const events = getEvents(receipt);
+        setComponentsFromEvents(contractComponents, events);
     }
 
     const mint_resources = async ({entity_id, resource_type, amount}: {entity_id: number.BigNumberish, resource_type: number.BigNumberish, amount: number.BigNumberish}) => {
         const tx = await execute("MintResources", [entity_id, resource_type, amount]);
-        await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const receipt = await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const events = getEvents(receipt);
+        setComponentsFromEvents(contractComponents, events);
     }
 
     const make_fungible_order = async ({maker_id, maker_entity_types, maker_quantities, taker_entity_types, taker_quantities}: {maker_id: number.BigNumberish, maker_entity_types: number.BigNumberish[], maker_quantities: number.BigNumberish[], taker_entity_types: number.BigNumberish[], taker_quantities: number.BigNumberish[]}) => {
@@ -39,59 +45,58 @@ export function createSystemCalls(
 
     const take_fungible_order = async({taker_id, trade_id}: {taker_id: number.BigNumberish, trade_id: number.BigNumberish}) => {
         const tx = await execute("TakeFungibleOrder", [taker_id, trade_id]);
-        await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const receipt = await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const events = getEvents(receipt);
+        setComponentsFromEvents(contractComponents, events);
     }
 
     const change_order_status = async({realm_id, trade_id, new_status}: {realm_id: number.BigNumberish, trade_id: number.BigNumberish, new_status: number.BigNumberish}) => {
         const tx = await execute("ChangeOrderStatus", [realm_id, trade_id, new_status]);
-        await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const receipt = await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const events = getEvents(receipt);
+        setComponentsFromEvents(contractComponents, events);
     }
 
     const create_free_transport_unit = async({realm_id, quantity}: {realm_id: number.BigNumberish, quantity: number.BigNumberish}): Promise<number> => {
         const tx = await execute("CreateFreeTransportUnit", [realm_id, quantity]);
-        await provider.provider.waitForTransaction(tx.transaction_hash, 500);
-        const events = await provider.provider.getTransactionReceipt(tx.transaction_hash).then((receipt) => {
-            return receipt.events.filter((event) => {
-                return event.keys.length === 1 &&
-                event.keys[0] === import.meta.env.VITE_EVENT_KEY;
-            })
-        })
+        const receipt = await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const events = getEvents(receipt);
+        setComponentsFromEvents(contractComponents, events);
+        // TODO: use getEntityIdFromEvents
         return parseInt(events[1].data[2])
     }
 
     const create_caravan = async({entity_ids}: {entity_ids: number.BigNumberish[]}): Promise<number> => {
         const tx = await execute("CreateCaravan", [entity_ids.length, ...entity_ids]);
-        await provider.provider.waitForTransaction(tx.transaction_hash, 500);
-        const events = await provider.provider.getTransactionReceipt(tx.transaction_hash).then((receipt) => {
-            return receipt.events.filter((event) => {
-                return event.keys.length === 1 &&
-                event.keys[0] === import.meta.env.VITE_EVENT_KEY;
-            })
-        })
+        const receipt = await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const events = getEvents(receipt);
+        setComponentsFromEvents(contractComponents, events);
+        // TODO: use getEntityIdFromEvents
         const caravan_id = parseInt(events[2].data[2]);
         return caravan_id;
     }
 
     const attach_caravan = async({realm_id, trade_id, caravan_id}: {realm_id: number.BigNumberish, trade_id: number.BigNumberish, caravan_id: number.BigNumberish}) => {
         const tx = await execute("AttachCaravan", [realm_id, trade_id, caravan_id]);
-        await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const receipt = await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const events = getEvents(receipt);
+        setComponentsFromEvents(contractComponents, events);
     }
 
     const claim_fungible_order = async({entity_id, trade_id}: {entity_id: number.BigNumberish, trade_id: number.BigNumberish}) => {
         const tx = await execute("ClaimFungibleOrder", [entity_id, trade_id]);
-        await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const receipt = await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const events = getEvents(receipt);
+        setComponentsFromEvents(contractComponents, events);
     }
 
     const create_realm = async({realm_id, owner, resource_types_packed, resource_types_count, cities, harbors, rivers, regions, wonder, order, position}: {realm_id: number.BigNumberish, owner: number.BigNumberish, resource_types_packed: number.BigNumberish, resource_types_count: number.BigNumberish, cities: number.BigNumberish, harbors: number.BigNumberish, rivers: number.BigNumberish, regions: number.BigNumberish, wonder: number.BigNumberish, order: number.BigNumberish, position: {x: number.BigNumberish, y: number.BigNumberish}}) => {
         const tx = await execute("CreateRealm", [realm_id, owner, resource_types_packed, resource_types_count, cities, harbors, rivers, regions, wonder, order, position.x, position.y]);
-        await provider.provider.waitForTransaction(tx.transaction_hash, 500);
-        const events = await provider.provider.getTransactionReceipt(tx.transaction_hash).then((receipt) => {
-            return receipt.events.filter((event) => {
-                return event.keys.length === 1 &&
-                event.keys[0] === import.meta.env.VITE_EVENT_KEY;
-            })
-        })
+        const receipt = await provider.provider.waitForTransaction(tx.transaction_hash, 500);
+        const events = getEvents(receipt);
+        setComponentsFromEvents(contractComponents, events);
         let realm_entity_id: number = 0;
+        // TODO: use getEntityIdFromEvents
         for (const event of events) {
             // if component change is Realm (hex), take entity_id
             if (event.data[0] === '0x5265616c6d') {
