486dddee8c111d6b3e09c8c54dbf72cea3afb796
aymericdelab <aymericdelabrousse@gmail.com>
2023-08-17 17:40:22 +0200

    fix: env

--- client/src/DojoContext.tsx
+++ client/src/DojoContext.tsx
@@ -6,36 +6,45 @@ import { useBurner } from "@dojoengine/create-burner";
 const DojoContext = createContext<SetupResult | null>(null);
 
 type Props = {
-    children: ReactNode;
-    value: SetupResult;
+  children: ReactNode;
+  value: SetupResult;
 };
 
 export const DojoProvider = ({ children, value }: Props) => {
-    const currentValue = useContext(DojoContext);
-    if (currentValue) throw new Error("DojoProvider can only be used once");
-    return <DojoContext.Provider value={value}>{children}</DojoContext.Provider>;
+  const currentValue = useContext(DojoContext);
+  if (currentValue) throw new Error("DojoProvider can only be used once");
+  return <DojoContext.Provider value={value}>{children}</DojoContext.Provider>;
 };
 
 export const useDojo = () => {
-    const value = useContext(DojoContext);
+  const value = useContext(DojoContext);
 
-    const provider = new RpcProvider({
-        nodeUrl: "http://localhost:5050",
-    });
+  const provider = new RpcProvider({
+    nodeUrl: import.meta.env.VITE_KATANA_URL,
+  });
 
-    const masterAccount = new Account(provider, import.meta.env.VITE_KATANA_ACCOUNT_1_ADDRESS!, import.meta.env.VITE_KATANA_ACCOUNT_1_PRIVATE_KEY!)
+  const masterAccount = new Account(
+    provider,
+    import.meta.env.VITE_KATANA_ACCOUNT_1_ADDRESS!,
+    import.meta.env.VITE_KATANA_ACCOUNT_1_PRIVATE_KEY!,
+  );
 
-    const { create, list, get, account, select, isDeploying } = useBurner(
-        {
-            masterAccount: masterAccount,
-            accountClassHash: import.meta.env.VITE_PUBLIC_ACCOUNT_CLASS_HASH!,
-            provider: provider
-        }
-    );
+  const { create, list, get, account, select, isDeploying } = useBurner({
+    masterAccount: masterAccount,
+    accountClassHash: import.meta.env.VITE_PUBLIC_ACCOUNT_CLASS_HASH!,
+    provider: provider,
+  });
 
-    if (!value) throw new Error("Must be used within a DojoProvider");
-    return {
-        setup: value,
-        account: { create, list, get, select, account: account ? account : masterAccount, isDeploying }
-    };
+  if (!value) throw new Error("Must be used within a DojoProvider");
+  return {
+    setup: value,
+    account: {
+      create,
+      list,
+      get,
+      select,
+      account: account ? account : masterAccount,
+      isDeploying,
+    },
+  };
 };
