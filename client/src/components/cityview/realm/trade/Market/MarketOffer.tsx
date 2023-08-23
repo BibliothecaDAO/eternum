809810e7e2eb0eb4098a9a0429812d925187b2fb
0x1337 <za6ah9t@icloud.com>
2023-08-23 13:26:44 +0700

    fix: resource grid styles

--- client/src/components/cityview/realm/trade/Market/MarketOffer.tsx
+++ client/src/components/cityview/realm/trade/Market/MarketOffer.tsx
@@ -71,10 +71,13 @@ export const MarketOffer = ({ marketOffer, onAccept }: TradeOfferProps) => {
       </div>
       <div className="flex items-end mt-2">
         <div className="flex items-center justify-around flex-1">
-          <div className="grid w-1/3 grid-cols-[repeat(3 ,auto)] gap-2 text-gold">
+          <div className="w-1/3 text-gold flex justify-center items-center flex-wrap">
             {resourcesGive &&
               resourcesGive.map(({ resourceId, amount }) => (
-                <div className="flex flex-col items-center" key={resourceId}>
+                <div
+                  className="flex flex-col items-center mx-2 my-1"
+                  key={resourceId}
+                >
                   <ResourceIcon
                     resource={findResourceById(resourceId)?.trait as any}
                     size="xs"
@@ -90,10 +93,13 @@ export const MarketOffer = ({ marketOffer, onAccept }: TradeOfferProps) => {
               resourcesGet &&
               calculateRatio(resourcesGive, resourcesGet).toFixed(2)}
           </div>
-          <div className="grid w-1/3 grid-cols-[repeat(3 ,auto)] gap-2 text-gold">
+          <div className="w-1/3 text-gold flex justify-center items-center flex-wrap">
             {resourcesGet &&
               resourcesGet.map(({ resourceId, amount }) => (
-                <div className="flex flex-col items-center" key={resourceId}>
+                <div
+                  className="flex flex-col items-center mx-2 my-1"
+                  key={resourceId}
+                >
                   <ResourceIcon
                     resource={findResourceById(resourceId)?.trait as any}
                     size="xs"
