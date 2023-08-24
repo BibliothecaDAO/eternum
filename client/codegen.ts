50c436b3d8199cefbdba3380b05e4b780200031e
0x1337 <za6ah9t@icloud.com>
2023-08-24 12:50:57 +0700

    fix: updated graphql

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,15 +1,19 @@
-import type { CodegenConfig } from '@graphql-codegen/cli';
- 
- const config: CodegenConfig = {
-    schema: 'http://localhost:8080',
-    documents: 'src/**/*.graphql',
-    generates: {
-        'src/generated/graphql.ts': {
-        plugins: ['typescript', 'typescript-operations', 'typescript-graphql-request'],
-        config: {
-            rawRequest: true
-        },
-        },
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "https://torii.w3clique.xyz/",
+  documents: "src/**/*.graphql",
+  generates: {
+    "src/generated/graphql.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-graphql-request",
+      ],
+      config: {
+        rawRequest: true,
+      },
     },
-    };
- export default config;
+  },
+};
+export default config;
