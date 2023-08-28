1bdb5bec4818e7b2957b14987c97b5f1d5340019
0x1337 <za6ah9t@icloud.com>
2023-08-29 01:50:31 +0700

    feat: interactive regions

--- client/src/components/NotificationsComponent.tsx
+++ client/src/components/NotificationsComponent.tsx
@@ -94,7 +94,9 @@ export const NotificationsComponent = ({
   }, []);
 
   return (
-    <div className={clsx("flex flex-col space-y-2", className)}>
+    <div
+      className={clsx("flex flex-col space-y-2 absolute right-0", className)}
+    >
       <Button
         variant="primary"
         onClick={() =>
