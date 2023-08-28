d80e3d2cffe886106128a54ef8d65efaa8040127
0x1337 <za6ah9t@icloud.com>
2023-08-28 23:07:19 +0700

    fix: donkey icon

--- client/src/components/cityview/realm/trade/Caravans/Caravan.tsx
+++ client/src/components/cityview/realm/trade/Caravans/Caravan.tsx
@@ -2,7 +2,7 @@ import React, { useMemo } from "react";
 import { OrderIcon } from "../../../../../elements/OrderIcon";
 import { ReactComponent as Pen } from "../../../../../assets/icons/common/pen.svg";
 import { ReactComponent as CaretDownFill } from "../../../../../assets/icons/common/caret-down-fill.svg";
-import { ReactComponent as DonkeyIcon } from "../../../../../assets/icons/units/donkey.svg";
+import { ReactComponent as DonkeyIcon } from "../../../../../assets/icons/units/donkey-circle.svg";
 import { ReactComponent as PremiumIcon } from "../../../../../assets/icons/units/premium.svg";
 
 import ProgressBar from "../../../../../elements/ProgressBar";
