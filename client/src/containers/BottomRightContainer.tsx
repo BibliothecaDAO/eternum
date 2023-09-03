f7e730a46973558702ab0224b480a11c7fb9eb8c
0x1337 <za6ah9t@icloud.com>
2023-09-03 23:03:01 +0700

    feat: expandable containers, chat

--- client/src/containers/BottomRightContainer.tsx
+++ client/src/containers/BottomRightContainer.tsx
@@ -2,13 +2,9 @@ import { ReactNode } from "react";
 import { BaseContainer } from "./BaseContainer";
 
 interface BottomRightContainerProps {
-    children?: ReactNode;
+  children?: ReactNode;
 }
 
 export const BottomRightContainer = ({ children }: BottomRightContainerProps) => {
-    return (
-        <BaseContainer className="absolute w-1/4 h-[550px] bottom-10 right-6" expandable>
-            {children}
-        </BaseContainer>
-    );
+  return <div className="absolute flex flex-col w-1/4 h-[550px] bottom-10 right-6">{children}</div>;
 };
