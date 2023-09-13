520035b21515501c8b3732ad5d7e51b826ef51ae
democritus <ponderingdemocritus@protonmail.com>
2023-09-13 14:38:23 +0100

    fix wallet and context

--- client/src/components/SignUpComponent.tsx
+++ client/src/components/SignUpComponent.tsx
@@ -9,28 +9,10 @@ import clsx from "clsx";
 
 type SignUpComponentProps = {};
 
-export const SignUpComponent = ({}: SignUpComponentProps) => {
-  const [canCreate, setCanCreate] = useState(true);
-
+export const SignUpComponent = ({ }: SignUpComponentProps) => {
   const {
-    account: { create, isDeploying, list, select },
+    account: { create, isDeploying, list, account, select, clear },
   } = useDojo();
-
-  const onReset = () => {
-    localStorage.removeItem("burners");
-    setCanCreate(true);
-  };
-
-  useEffect(() => {
-    if (list().length > 0) {
-      setCanCreate(false);
-    }
-  }, []);
-
-  const onCreate = () => {
-    create();
-    setCanCreate(false);
-  };
 
   const [showSignupPopup, setShowSignupPopup] = useState(true);
   const setShowBlurOverlay = useUIStore((state) => state.setShowBlurOverlay);
@@ -51,22 +33,18 @@ export const SignUpComponent = ({}: SignUpComponentProps) => {
         <div className="flex flex-col items-center p-3">
           <img src="/images/eternum-logo.svg" className=" w-48" alt="Eternum Logo" />
           <img src="/images/buildings/storehouse.jpg" className="w-full my-3" alt="Eternum Logo" />
-          <div className={clsx("flex mx-2", !canCreate && "mb-2")}>
-            {/* className={clsx("ml-auto p-2 !h-4 text-xxs !rounded-md", !canAccept && "mb-4")} */}
-            <Button disabled={!canCreate} variant={"primary"} onClick={onCreate}>
+          <div className={clsx("flex mx-2")}>
+            <Button variant={"primary"} onClick={create}>
               {isDeploying ? "deploying burner" : "create burner"}
             </Button>
-            <Button variant={"danger"} onClick={onReset}>
+            <Button variant={"danger"} onClick={clear}>
               {"reset burners"}
             </Button>
           </div>
-          {!canCreate && (
-            <div className="text-xxs text-order-giants/70 w-min whitespace-nowrap">Can deploy maximum 1 burner</div>
-          )}
           <div className="border border-gold my-3 w-full rounded-lg bg-black  flex p-2 text-white">
             <div className="px-2">signer: </div>
 
-            <select className={"w-full bg-black"} onChange={(e) => select(e.target.value)}>
+            <select value={account.address} className={"w-full bg-black"} onChange={(e) => select(e.target.value)}>
               {list().map((account, index) => {
                 return (
                   <option value={account.address} key={index}>
