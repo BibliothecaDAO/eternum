b071f524885cbde5190db74b919d0d9bc92840c4
0x1337 <za6ah9t@icloud.com>
2023-09-16 00:28:14 +0700

    feat: draggable popups save position

--- client/src/elements/SecondaryPopup.tsx
+++ client/src/elements/SecondaryPopup.tsx
@@ -1,27 +1,59 @@
 import clsx from "clsx";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Draggable from "react-draggable";
 
 type FilterPopupProps = {
   children: React.ReactNode;
   className?: string;
+  name?: string;
 };
 
-export const SecondaryPopup = ({ children, className }: FilterPopupProps) => {
+export const SecondaryPopup = ({ children, className, name }: FilterPopupProps) => {
   const nodeRef = React.useRef(null);
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [loaded, setLoaded] = useState(false);
+
+  const handleStop = (e: any, data: any) => {
+    if (name) {
+      localStorage.setItem(name, JSON.stringify({ x: data.x, y: data.y }));
+    }
+  };
+
+  useEffect(() => {
+    if (name) {
+      const pos = localStorage.getItem(name);
+      if (pos) {
+        setPosition(JSON.parse(pos));
+      }
+    }
+    setLoaded(true);
+  }, []);
+
   return (
-    <Draggable nodeRef={nodeRef}>
-      <div ref={nodeRef} className={clsx("fixed z-50 flex flex-col translate-x-6 top-[200px] left-[450px]", className)}>
-        {children}
-      </div>
-    </Draggable>
+    <>
+      {loaded && (
+        <Draggable handle=".handle" defaultPosition={position} nodeRef={nodeRef} onStop={handleStop}>
+          <div
+            ref={nodeRef}
+            className={clsx("fixed z-50 flex flex-col translate-x-6 top-[200px] left-[450px]", className)}
+          >
+            {children}
+          </div>
+        </Draggable>
+      )}
+    </>
   );
 };
 
-SecondaryPopup.Head = ({ children }: { children: React.ReactNode }) => (
-  <div className="text-xxs relative -mb-[1px] z-10 bg-brown px-1 py-0.5 rounded-t-[4px] border-t border-x border-white text-white w-min whitespace-nowrap">
-    {" "}
-    {children}{" "}
+SecondaryPopup.Head = ({ children, className }: { children: React.ReactNode; className?: string }) => (
+  <div
+    className={clsx(
+      "text-xxs relative cursor-move -mb-[1px] z-30 bg-brown px-1 py-0.5 rounded-t-[4px] border-t border-x border-white text-white w-min whitespace-nowrap handle",
+      className,
+    )}
+  >
+    {children}
   </div>
 );
 
@@ -29,7 +61,7 @@ SecondaryPopup.Body = ({ width = null, children }: { width?: string | null; chil
   <div
     className={`${
       width ? "" : "min-w-[438px]"
-    } relative z-0 bg-gray border flex flex-col border-white rounded-tr-[4px] rounded-b-[4px]`}
+    } relative z-10 bg-gray border flex flex-col border-white rounded-tr-[4px] rounded-b-[4px]`}
     style={{ width: width ? width : "" }}
   >
     {children}
