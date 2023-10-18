103a87572d17e0c11b54852b9df1294d7fd3f8e6
aymericdelab <aymericdelabrousse@gmail.com>
2023-10-18 18:24:55 +0200

    feat: added syscalls

--- sdk/packages/eternum/src/index.ts
+++ sdk/packages/eternum/src/index.ts
@@ -1,26 +1,3 @@
-import { RPCProvider } from "@dojoengine/core";
-import { Contracts } from "./constants"
-import { PurchaseLaborProps } from "./types";
-
-export class EternumProvider extends RPCProvider {
-    constructor(world_address: string, url?: string) {
-        super(world_address, url);
-    }
-
-    public async purchase_labor(props: PurchaseLaborProps): Promise<any> {
-        const { signer, entity_id, resource_type, labor_units } = props;
-
-        const tx = await this.execute(signer, {
-            contractAddress: Contracts.LABOR_SYSTEMS,
-            calldata: {
-                world: this.getWorldAddress(),
-                entity_id,
-                resource_type,
-                labor_units,
-            },
-            entrypoint: "purchase",
-        });
-
-        return await this.provider.waitForTransaction(tx.transaction_hash, { retryInterval: 500 });
-    }
-}
+export * from "./types";
+export * from "./constants";
+export * from "./provider";
