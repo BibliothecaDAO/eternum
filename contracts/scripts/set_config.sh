dcec20675fc6c7e286c9ae59b850506e70c4b199
Yangqijun <40876894+Again1013@users.noreply.github.com>
2023-10-22 18:47:43 +0800

    sh

--- contracts/scripts/set_config.sh
+++ contracts/scripts/set_config.sh
@@ -269,9 +269,9 @@ pushd $(dirname "$0")/..
 
 export RPC_URL="http://localhost:5050";
 
-export WORLD_ADDRESS=$(cat ../target/dev/manifest.json | jq -r '.world.address')
+export WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
 
-export ACTIONS_ADDRESS=$(cat ../target/dev/manifest.json | jq -r '.contracts[] | select(.name == "cc" ).address')
+export ACTIONS_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "cc" ).address')
 
 echo "---------------------------------------------------------------------------"
 echo world : $WORLD_ADDRESS
