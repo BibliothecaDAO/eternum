50d0f3816a4aa45270fb5700ae078b2494db56e4
raschel <38816784+aymericdelab@users.noreply.github.com>
2023-11-13 08:45:17 +0100

    Update labor.ts

--- sdk/packages/eternum/src/constants/labor.ts
+++ sdk/packages/eternum/src/constants/labor.ts
@@ -1,7 +1,7 @@
 export const LABOR_CONFIG_ID = 999999999999999997n;
 
 export const LABOR_CONFIG = {
-  base_food_per_cycle: 14000000,
+  base_food_per_cycle: 21000,
   base_labor_units: 7200,
   base_resources_per_cycle: 21000,
 };
