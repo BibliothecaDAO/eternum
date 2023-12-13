c75254c93e60abd6452e94b653c66cb080391a5c
Timothy Edison <timothyedison@hotmail.com>
2023-12-13 15:59:13 +0100

    Add basic UI for villagers townhall meetings

--- client/src/ui/components/cityview/realm/villagers/NpcChat.tsx
+++ client/src/ui/components/cityview/realm/villagers/NpcChat.tsx
@@ -1,167 +1,77 @@
 import { useEffect, useRef, useState } from "react";
-import { random } from "@latticexyz/utils";
-import { nameFromEntityId } from "./utils";
-import { Npc } from "./types";
+import useWebSocket from "react-use-websocket";
 import NpcChatMessage from "./NpcChatMessage";
-import GptInterface from "../../../../utils/NpcPrompt";
 import { NpcChatMessageProps } from "./NpcChatMessage";
-import { useNpcs } from "../../../../NpcContext";
-import { Role } from "./constants";
 
 interface NpcChatProps {
-  npcs: Npc[];
+  spawned: number;
   realmId: number;
 }
 
 // Store chat history in this ;
-const NpcChat = ({ npcs, realmId }: NpcChatProps) => {
-  const { genMsg, setGenMsg, type } = useNpcs();
+const NpcChat = ({ spawned, realmId }: NpcChatProps) => {
   const chatIdentifier: string = `npc_chat_${realmId}`;
-
-  const [messageList, setMessageList] = useState<NpcChatMessageProps[]>(
-    JSON.parse(window.localStorage.getItem(chatIdentifier)),
-  );
-
-  //   Can remove this I think
-  const [ready, setReady] = useState<boolean>(false);
   const bottomRef = useRef<HTMLDivElement>(null);
+  const [messageList, setMessageList] = useState<NpcChatMessageProps[]>(
+    JSON.parse(window.localStorage.getItem(chatIdentifier)) ?? [],
+  );
+  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(import.meta.env.VITE_OVERLORE_WS_URL, {
+    share: false,
+    shouldReconnect: () => true,
+  });
 
-  const gptInterface = new GptInterface();
-
+  // Runs when a new WebSocket message is received (lastJsonMessage)
   useEffect(() => {
-    const generateMessages = async () => {
-      if (npcs.length == 0) {
-        setGenMsg(false);
-        return;
-      }
-      let npc;
-      let systemMessage: NpcChatMessageProps = { sender: "Sytem message", message: "" };
-      let harvest = true;
-      // loading!
-      if (type === "random") {
-        // random is from the button
-        npc = npcs[random(npcs.length - 1)];
-        harvest = false;
-      } else if (type === "farmer") {
-        // this is from a harvest, so add a message before the actual villager's prompt
-        const farmers = npcs.filter((npc) => npc.role === Role.Farmer);
-        if (farmers.length === 0) {
-          setGenMsg(false);
-          let newMessages = messageList || [];
-          systemMessage.message = `No farmer in your village can talk to you.`;
-          newMessages.push(systemMessage);
-          setMessageList(newMessages);
-          window.localStorage.setItem(chatIdentifier, JSON.stringify(newMessages));
-          return;
-        }
-        console.log(farmers);
-        npc = farmers[random(farmers.length - 1)];
-        console.log(npc);
-        systemMessage.message = `You harvested wheat`;
-      } else if (type === "miner") {
-        // this is from a harvest
-        const miners = npcs.filter((npc) => npc.role === Role.Miner);
-        npc = miners[random(miners.length - 1)];
-        systemMessage.message = `You mined`;
-        if (miners.length !== 0) {
-          setGenMsg(false);
-          return;
-        }
-      }
-      let response: Response;
-      // call chatGTP here
-      if (harvest) {
-        response = await gptInterface.generateHarvestPrompt(npc);
-      } else {
-        response = await gptInterface.generateGreetingsPrompt(npc);
-      }
-      const data = await response.json();
-      const generatedPrompt = data.choices[0].message.content.trim();
-
-      const sender = nameFromEntityId(npc.entityId, npc.sex);
-
-      let newMessages = messageList || [];
-
-      if (systemMessage.message !== "") {
-        newMessages.push(systemMessage);
-      }
-
-      newMessages.push({ message: generatedPrompt, sender });
-      setMessageList(newMessages);
-
-      window.localStorage.setItem(chatIdentifier, JSON.stringify(newMessages));
-
-      setTimeout(() => {
-        if (bottomRef.current) {
-          bottomRef.current.scrollIntoView({ behavior: "smooth", block: "nearest" });
-        }
-      }, 1);
-      setGenMsg(false);
-    };
-    if (!genMsg) {
+    if (lastJsonMessage === null) {
       return;
     }
-    generateMessages();
-  }, [genMsg]);
+    let msgsArray: string[] = JSON.parse(JSON.stringify(lastJsonMessage, null, 2)).split("\n\n");
+    if (msgsArray[msgsArray.length - 1] === "") msgsArray.pop();
+    const newMessages = msgsArray.map((msg) => {
+      const nameMsg = msg.split(":");
+      return { sender: nameMsg[0], message: nameMsg[1] };
+    });
+    const newArray = [...messageList, ...newMessages];
+    localStorage.setItem(chatIdentifier, JSON.stringify(newArray));
+    setMessageList(newArray);
+    setTimeout(() => {
+      if (bottomRef.current) {
+        bottomRef.current.scrollIntoView({ behavior: "smooth", block: "nearest" });
+      }
+    }, 1);
+  }, [lastJsonMessage]);
 
   useEffect(() => {
-    setReady(true);
-  }, []);
+    if (spawned === -1) {
+      return;
+    }
 
-  // const createRoom = async (groupName: string) => {
-  //     client?.channel.createRoom({
-  //         groupName, permissions: {
-  //             "group:join": {
-  //                 type: "enum",
-  //                 value: "public"
-  //             }
-  //         }
-  //     })
-  // }
+    sendJsonMessage({
+      // Replace with this after demo version
+      // user: realm.realm_id,
+      user: 0,
+      day: spawned,
+    });
+  }, [spawned]);
+
+  useEffect(() => {
+    console.log("Connection state changed");
+  }, [readyState]);
 
   useEffect(() => {}, []);
   return (
     <div className="relative flex flex-col h-full overflow-auto">
-      <div className={"text-white text-xxs pr-2 mt-1"} style={{ position: "static", textAlign: "right" }}>
-        NPCs: {npcs.length} / 5
-      </div>
-
       <div
         className={
           "relative flex flex-col h-full overflow-auto relative top-3 flex flex-col h-full center mx-auto w-[96%] mb-3 overflow-auto border border-gold"
         }
       >
-        {genMsg ? (
-          <div className="absolute h-full bg-black w-[100%] text-white text-center flex justify-center">
-            <div className="self-center">
-              <img src="/images/eternum-logo_animated.png" className="invert scale-50" />
-            </div>
-          </div>
-        ) : (
-          <>
-            {messageList?.map((message, index) => {
-              return <NpcChatMessage key={index} {...message} />;
-            })}
-            {/* <div className="h-[30px] w-[30px] text-white text-center flex justify-center">
-              <div className="self-center">
-                <img src="/images/eternum-logo_animated.png" className="invert" />
-              </div>
-            </div> */}
-            {/* <div className="flex flex-col px-2 mb-3 select-none py-1">
-              <div className="flex items-center">
-                <div className="flex flex-col w-full">
-                  <div className="h-[30px] w-[30px] text-white text-center flex justify-center">
-                    <div className="self-center">
-                      <img src="/images/eternum-logo_animated.png" className="invert" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div> */}
-
-            <span className="" ref={bottomRef}></span>
-          </>
-        )}
+        <>
+          {messageList?.map((message, index) => {
+            return <NpcChatMessage key={index} {...message} />;
+          })}
+          <span className="" ref={bottomRef}></span>
+        </>
       </div>
     </div>
   );
--- client/src/ui/components/cityview/realm/villagers/NpcPanel.tsx
+++ client/src/ui/components/cityview/realm/villagers/NpcPanel.tsx
@@ -1,53 +1,16 @@
 import { useEffect, useMemo, useState } from "react";
 import Button from "../../../../elements/Button";
 import NpcChat from "./NpcChat";
-import { Npc } from "./types";
 import useRealmStore from "../../../../hooks/store/useRealmStore";
 import { useRoute } from "wouter";
 import { getRealm } from "../../../../utils/realms";
-import { HasValue, getComponentValue, runQuery } from "@latticexyz/recs";
-import { useDojo } from "../../../../DojoContext";
-import { getRandomMood, parseMoodFeltToStruct } from "./utils";
-import { random } from "@latticexyz/utils";
-import { useNpcs } from "../../../../NpcContext";
-import useWebSocket, { ReadyState } from "react-use-websocket";
 
 type NpcPanelProps = {
   type?: "all" | "farmers" | "miners";
 };
 
 export const NpcPanel = ({ type = "all" }: NpcPanelProps) => {
-  const {
-    setup: {
-      components: { Npc: NpcComponent },
-      systemCalls: { spawn_npc, change_mood },
-      // Not using this as the optimistic function isn't implemented
-      optimisticSystemCalls: { optimisticSpawnNpc },
-    },
-    account: { account },
-  } = useDojo();
-
-  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(import.meta.env.VITE_OVERLORE_WS_URL, {
-    share: false,
-    shouldReconnect: () => true,
-  });
-
-  useEffect(() => {
-    console.log("Connection state changed");
-  }, [readyState]);
-
-  // Run when a new WebSocket message is received (lastJsonMessage)
-  useEffect(() => {
-    console.log(`Got a new message: ${JSON.stringify(lastJsonMessage, null, 2)}`);
-  }, [lastJsonMessage]);
-
-  const [spawned, setSpawned] = useState(false);
-  const { setGenMsg, setType } = useNpcs();
-  // @ts-ignore
-  // TODO remove any
-  const [match, params]: any = useRoute("/realm/:id/:tab");
-
-  useEffect(() => {}, [params]);
+  const [spawned, setSpawned] = useState(-1);
 
   const { realmEntityId } = useRealmStore();
 
@@ -55,110 +18,21 @@ export const NpcPanel = ({ type = "all" }: NpcPanelProps) => {
     return realmEntityId ? getRealm(realmEntityId) : undefined;
   }, [realmEntityId]);
 
-  let npcs = useMemo(() => {
-    if (realm) {
-      const entityIds = runQuery([HasValue(NpcComponent, { realm_id: realm.realm_id })]);
-      let npcs: Npc[] = Array.from(entityIds).map((entityId) => {
-        let npc = getComponentValue(NpcComponent, entityId);
-        return { ...npc, entityId, mood: parseMoodFeltToStruct(npc.mood) };
-      });
-      setSpawned(false);
-      return npcs;
-    } else {
-      return [];
-    }
-  }, [spawned]);
-
-  const genTownHall = async () => {
-    // Optimistic call for now is useless as the normal call work completely fine
-    // await optimisticSpawnNpc(spawn_npc)({ signer: account, realm_id: realm.realm_id });
-    if (readyState === ReadyState.OPEN) {
-      sendJsonMessage({
-        // Replace with this after demo version
-        // user: realm.realm_id,
-        user: 0,
-        day: 0,
-      });
-    }
-    setSpawned(true);
-  };
-
-  const randomizeMood = async () => {
-    console.log("randomizing NPC");
-    const npc = npcs[random(npcs.length - 1, 0)];
-    // Should we do optimistic call?
-    await change_mood({
-      realm_id: npc.realm_id.valueOf(),
-      npc_id: npc.entityId,
-      mood: getRandomMood(),
-      signer: account,
-    });
-  };
-
-  const generateVillagerMessage = () => {
-    setGenMsg(true);
-    setType("random");
-  };
-
   return (
     <div className="flex flex-col h-[250px] relative pb-3">
-      {/* <SortPanel className="px-3 py-2">
-        {sortingParams.map(({ label, sortKey, className }) => (
-          <SortButton
-            className={className}
-            key={sortKey}
-            label={label}
-            sortKey={sortKey}
-            activeSort={activeSort}
-            onChange={(_sortKey, _sort) => {
-              setActiveSort({
-                sortKey: _sortKey,
-                sort: _sort,
-              });
-            }}
-          />
-        ))}
-      </SortPanel> */}
-      {/* {realm &&
-        realmResourceIds.map((resourceId) => (
-          <div className="flex flex-col p-2" key={resourceId}>
-            <NpcComponent
-              //   onBuild={() => {
-              //     buildResource == resourceId ? setBuildResource(null) : setBuildResource(resourceId);
-              //   }}
-              entityId={resourceId}
-              realm={realm}
-              //   setBuildLoadingStates={setBuildLoadingStates}
-              //   buildLoadingStates={buildLoadingStates}
-            />
-          </div>
-        ))} */}
-      <div className="flex flex-row w-[100%] items-center space-y-2" style={{ justifyContent: "center" }}>
+      <div
+        className="flex flex-row w-[100%] items-center space-y-2"
+        style={{ position: "relative", justifyContent: "center", top: "2%" }}
+      >
         <Button
-          disabled={npcs.length >= 5}
-          className="mx-1 top-3 sticky w-32 bottom-2 !rounded-full"
-          onClick={() => genTownHall()}
+          className="mx-1 top-3 left-3 w-32 bottom-2 !rounded-full"
+          onClick={() => setSpawned(spawned + 1)}
           variant="primary"
         >
-          {spawned ? "Townhall is generated" : "Generate townhall"}
-        </Button>
-        <Button
-          className="mx-1 top-3 left-3 sticky w-32 bottom-2 !rounded-full"
-          onClick={generateVillagerMessage}
-          disabled={npcs.length == 0}
-          variant="primary"
-        >
-          👋 Greet villagers!
-        </Button>
-        <Button
-          className="mx-1 top-3 left-3 sticky w-32 bottom-2 !rounded-full"
-          onClick={() => randomizeMood()}
-          variant="primary"
-        >
-          Randomize mood
+          Gather villagers
         </Button>
       </div>
-      <NpcChat npcs={npcs} realmId={realm.realm_id} />
+      <NpcChat spawned={spawned} realmId={realm.realm_id} />
     </div>
   );
 };
