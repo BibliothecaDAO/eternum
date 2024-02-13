ffece8502f836ef34142251e4544dd16c5608634
0x1337 <za6ah9t@icloud.com>
2024-02-14 01:01:02 +0700

    fix: attempt to fix build errors

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), wasm(), topLevelAwait(), splitVendorChunkPlugin()],
+  plugins: [svgr(), react(), wasm(), topLevelAwait()],
   resolve: {
     alias: {
       events: "events",
@@ -20,6 +20,26 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         map: resolve(__dirname, "map/index.html"),
         realm: resolve(__dirname, "realm/index.html"),
+      },
+      maxParallelFileOps: 2,
+      cache: false,
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+        sourcemap: true,
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+        inlineDynamicImports: false,
+        sourcemapIgnoreList: (relativeSourcePath) => {
+          const normalizedPath = path.normalize(relativeSourcePath);
+          return normalizedPath.includes("node_modules");
+        },
       },
     },
   },
