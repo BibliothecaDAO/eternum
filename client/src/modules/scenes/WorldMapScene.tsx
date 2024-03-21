fb12890af10adb03234fef60492002c117191d1a
0x1337 <za6ah9t@icloud.com>
2024-03-21 15:15:51 +0700

    fix: removed old code

--- client/src/modules/scenes/WorldMapScene.tsx
+++ client/src/modules/scenes/WorldMapScene.tsx
@@ -1,9 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useRoute } from "wouter";
 import * as THREE from "three";
 
 import useUIStore from "../../hooks/store/useUIStore.js";
-import { useResources } from "../../hooks/helpers/useResources.js";
 
 import HighlightedHexes from "../../components/worldmap/hexagon/HighlightedHexes.js";
 import { WorldMap } from "../../components/worldmap/hexagon/WorldHexagon";
@@ -31,10 +30,6 @@ const StarsSky = () => {
 };
 
 export const WorldMapScene = () => {
-  const worldRef = useRef();
-
-  const hyperstructures = useUIStore((state) => state.hyperstructures);
-  const { getCaravansWithResourcesChest } = useResources();
   const [isMapView] = useRoute("/map");
   const showBlankOverlay = useUIStore((state) => state.showBlankOverlay);
 
@@ -46,77 +41,15 @@ export const WorldMapScene = () => {
     }, 300);
   }, []);
 
-  // const { getCaravanInfo } = useCaravan();
-  // const caravanIds = getCaravansWithResourcesChest();
-  // const { realmEntityId } = useRealmStore();
-  // const { realm } = useGetRealm(realmEntityId);
-
-  // const destinations = useMemo(() => {
-  //   if (!realm) return [];
-  //   return caravanIds
-  //     .map((caravanId) => {
-  //       const { destination: from } = getCaravanInfo(caravanId);
-  //       if (from) {
-  //         return {
-  //           from: getUIPositionFromContractPosition(from),
-  //           to: getUIPositionFromContractPosition(realm.position),
-  //         };
-  //       }
-  //     })
-  //     .filter(Boolean);
-  // }, [caravanIds, realm]);
-
   return (
     <>
-      {/* <Flags /> */}
-      {/* <TransformControls mode="translate" onChange={(e) => console.log(e?.target?.object?.position)}>
-        <mesh>
-          <boxGeometry args={[10, 10, 10]} />
-          <meshBasicMaterial color="red" />
-        </mesh>
-      </TransformControls> */}
-      {/* <WorldMap ref={worldRef} /> */}
-      {/* <HyperstructureStarted /> */}
       {!showBlankOverlay && isMapView && <WorldMap />}
       {/* <StarsSky /> */}
       <HighlightedHexes />
-      {/* <mesh rotation={[-Math.PI / 2, 0, 0]} position={[1334.1, 0.1, -695.175]}>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[1334.1, 0.1, -695.175]}>
         <planeGeometry args={[2668, 1390.35]} />
-        <meshBasicMaterial color="black" transparent opacity={1} />
-      </mesh> */}
-
-      {/* {hyperstructures.map((hyperstructure, i) => {
-        if (hyperstructure) {
-          if (hyperstructure.completed) {
-            let uiPosition = hyperstructure.uiPosition;
-            return <HyperstructureFinished key={i} scale={1.8} position={[uiPosition.x, 15, -uiPosition.y]} />;
-          } else if (hyperstructure.progress > 50) {
-            return (
-              <HyperstructureHalf
-                key={i}
-                scale={100}
-                hyperstructure={hyperstructure}
-                position={[hyperstructure.uiPosition.x, hyperstructure.uiPosition.y, hyperstructure.uiPosition.z]}
-              />
-            );
-          } else {
-            return (
-              <HyperstructureStarted
-                key={i}
-                hyperstructure={hyperstructure}
-                scale={5}
-                // position={[hyperstructure.uiPosition.x, hyperstructure.uiPosition.y, hyperstructure.uiPosition.z]}
-                position={[0, 0, 0]}
-              />
-            );
-          }
-        }
-        return null;
-      })} */}
-      {/* {banks.map((bank, i) => {
-        return <Bank key={i} position={[bank.x, bank.y, bank.z]} />;
-      })} */}
-      {/* <Arcs paths={destinations} /> */}
+        <meshBasicMaterial color="#fff" transparent opacity={1} />
+      </mesh>
     </>
   );
 };
