f18a668086a663d0470893e4082f6dc2f39339a7
0x1337 <za6ah9t@icloud.com>
2024-03-22 13:14:19 +0700

    fix: disable old travel popup

--- client/src/components/worldmap/WorldPopups.tsx
+++ client/src/components/worldmap/WorldPopups.tsx
@@ -31,7 +31,6 @@ export const WorldPopups = () => {
       {showFeedPopup && selectedHyperstructure && (
         <FeedHyperstructurePopup selectedHyperstructure={selectedHyperstructure} onClose={onCloseHyperstructure} />
       )}
-      {selectedEntity && <ChooseArmyActionPopup />}
     </div>
   );
 };
