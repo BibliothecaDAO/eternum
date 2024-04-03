b36a4e80d2e527d03b64e6ad6d24f6fd69d81de6
0x1337 <za6ah9t@icloud.com>
2024-04-03 19:26:20 +0700

    feat: refactor, reject building if cell isn't empty

--- client/src/components/cityview/hexception/BuildArea.tsx
+++ client/src/components/cityview/hexception/BuildArea.tsx
@@ -1,15 +1,14 @@
 import { useGLTF } from "@react-three/drei";
 import useUIStore from "../../../hooks/store/useUIStore";
-import HexBuildGrid from "./HexBuildGrid";
+import HexBuildGrid, { hasBuilding } from "./HexBuildGrid";
 import * as THREE from "three";
 import { getUIPositionFromColRow } from "../../../utils/utils";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
 const BuildArea = () => {
   const hoveredBuildHex = useUIStore((state) => state.hoveredBuildHex);
   const buildMode = useUIStore((state) => state.buildMode);
   const builtCastles = useUIStore((state) => state.builtCastles);
-  const setBuiltCastles = useUIStore((state) => state.setBuiltCastles);
 
   const castleCoords = useMemo(() => {
     return getUIPositionFromColRow(hoveredBuildHex.col, hoveredBuildHex.row, true);
@@ -19,7 +18,7 @@ const BuildArea = () => {
     <group>
       {buildMode && (
         <group position={[castleCoords.x, 2.32, -castleCoords.y]}>
-          <Castle transparent />
+          <Castle transparent color={hasBuilding(hoveredBuildHex.col, hoveredBuildHex.row) ? "red" : "green"} />
         </group>
       )}
       {builtCastles.map((castle) => {
@@ -37,11 +36,21 @@ const BuildArea = () => {
 
 export default BuildArea;
 
-const Castle = ({ transparent }: { transparent?: boolean }) => {
+const Castle = ({ transparent, color = "green" }: { transparent?: boolean; color?: string }) => {
   const { nodes, materials } = useGLTF("/models/realm-buildings-transformed.glb");
   const transparentMaterial = new THREE.MeshStandardMaterial();
-  transparentMaterial.transparent = true;
-  transparentMaterial.opacity = 0.5;
+  const _color = new THREE.Color(color);
+
+  if (transparent) {
+    transparentMaterial.color = _color;
+    transparentMaterial.transparent = true;
+    transparentMaterial.opacity = 0.5;
+  }
+
+  useEffect(() => {
+    _color.setColorName(color);
+    transparentMaterial.color = _color;
+  }, [color]);
 
   return (
     <mesh
--- client/src/components/cityview/hexception/HexBuildGrid.tsx
+++ client/src/components/cityview/hexception/HexBuildGrid.tsx
@@ -5,84 +5,120 @@ import { HEX_RADIUS } from "../../worldmap/hexagon/WorldHexagon";
 import { getUIPositionFromColRow } from "../../../utils/utils";
 import { Html } from "@react-three/drei";
 
+export const hasBuilding = (col: number, row: number) => {
+  const { builtCastles } = useUIStore((state) => ({
+    buildMode: state.buildMode,
+    hoveredBuildHex: state.hoveredBuildHex,
+    setHoveredBuildHex: state.setHoveredBuildHex,
+    builtCastles: state.builtCastles,
+    setBuiltCastles: state.setBuiltCastles,
+  }));
+
+  return builtCastles.some((castle) => castle.col === col && castle.row === row);
+};
+
 const HexBuildGrid = () => {
   const hexagonGeometry = new THREE.ShapeGeometry(createHexagonShape(HEX_RADIUS));
 
-  const _color = new THREE.Color("gray");
-  const center = { col: 4, row: 4 };
+  const hexPositions = generateHexPositions();
+  const { buildMode, hoveredBuildHex, setHoveredBuildHex, builtCastles, setBuiltCastles } = useUIStore((state) => ({
+    buildMode: state.buildMode,
+    hoveredBuildHex: state.hoveredBuildHex,
+    setHoveredBuildHex: state.setHoveredBuildHex,
+    builtCastles: state.builtCastles,
+    setBuiltCastles: state.setBuiltCastles,
+  }));
 
-  const generateHexPositions = (center: { col: number; row: number }) => {
-    const addOffset = center.row % 2 === 0 && center.row > 0 ? 0 : 1;
-    const radius = 4;
-    const positions = [] as any[];
-    const normalizedCenter = { col: 4, row: 4 };
-    const shifted = { col: center.col - normalizedCenter.col, row: center.row - normalizedCenter.row };
-    for (let _row = normalizedCenter.row - radius; _row <= normalizedCenter.row + radius; _row++) {
-      const basicCount = 9;
-      const decrease = Math.abs(_row - radius);
-      const colsCount = basicCount - decrease;
-      let startOffset = _row % 2 === 0 ? (decrease > 0 ? Math.floor(decrease / 2) : 0) : Math.floor(decrease / 2);
-      if (addOffset > 0 && _row % 2 !== 0) {
-        if (center.row < 0 && center.row % 2 === 0) startOffset += 1;
-      }
-      for (
-        let _col = startOffset + normalizedCenter.col - radius;
-        _col < normalizedCenter.col - radius + colsCount + startOffset;
-        _col++
-      ) {
-        positions.push({
-          ...getUIPositionFromColRow(_col + shifted.col, _row + shifted.row, true),
-          z: 0.32,
-          color: _color,
-          col: _col + shifted.col,
-          row: _row + shifted.row,
-          startOffset: startOffset,
-        });
-      }
-    }
-
-    return positions;
-  };
-
-  const hexPositions = generateHexPositions(center);
-  const buildMode = useUIStore((state) => state.buildMode);
-  const hoveredBuildHex = useUIStore((state) => state.hoveredBuildHex);
-  const setHoveredBuildHex = useUIStore((state) => state.setHoveredBuildHex);
-  const builtCastles = useUIStore((state) => state.builtCastles);
-  const setBuiltCastles = useUIStore((state) => state.setBuiltCastles);
+  // Check if a building exists at the given position
 
   return (
     <group rotation={[Math.PI / -2, 0, 0]} position={[0, 2, 0]}>
-      {hexPositions.map((hexPosition, index) => {
-        return (
-          <group
-            key={index}
-            position={[hexPosition.x, hexPosition.y, hexPosition.z]}
-            onPointerMove={(e) => {
-              if (buildMode) {
-                setHoveredBuildHex({ col: hexPosition.col, row: hexPosition.row });
-              }
-            }}
-            onClick={() => {
-              if (buildMode) {
-                setBuiltCastles([...builtCastles, { col: hexPosition.col, row: hexPosition.row }]);
-              }
-            }}
-          >
-            <mesh geometry={hexagonGeometry}>
-              <meshMatcapMaterial
-                color={
-                  hoveredBuildHex.col === hexPosition.col && hoveredBuildHex.row === hexPosition.row && buildMode
-                    ? "limegreen"
-                    : _color
-                }
-              />
-            </mesh>
-          </group>
-        );
-      })}
+      {hexPositions.map((hexPosition, index) => (
+        <Hexagon
+          key={index}
+          position={hexPosition}
+          geometry={hexagonGeometry}
+          color={getColorForHexagon(hexPosition, buildMode, hoveredBuildHex)}
+          onPointerMove={() => buildMode && setHoveredBuildHex({ col: hexPosition.col, row: hexPosition.row })}
+          onClick={() =>
+            buildMode &&
+            !hasBuilding(hexPosition.col, hexPosition.row) &&
+            setBuiltCastles([...builtCastles, { col: hexPosition.col, row: hexPosition.row }])
+          }
+        />
+      ))}
     </group>
   );
+};
+
+const Hexagon = ({
+  position,
+  geometry,
+  color,
+  onPointerMove,
+  onClick,
+}: {
+  position: any;
+  geometry: any;
+  color: any;
+  onPointerMove: any;
+  onClick: any;
+}) => (
+  <mesh
+    position={[position.x, position.y, position.z]}
+    onPointerMove={onPointerMove}
+    onClick={onClick}
+    geometry={geometry}
+  >
+    <meshMatcapMaterial color={color} />
+  </mesh>
+);
+
+const getColorForHexagon = (hexPosition: any, buildMode: any, hoveredBuildHex: any) => {
+  if (buildMode) {
+    if (hoveredBuildHex.col === hexPosition.col && hoveredBuildHex.row === hexPosition.row) {
+      if (hasBuilding(hexPosition.col, hexPosition.row)) {
+        return "red";
+      }
+      return "limegreen";
+    }
+  }
+  return "gray";
+};
+
+const generateHexPositions = () => {
+  const _color = new THREE.Color("gray");
+  const center = { col: 4, row: 4 };
+  const addOffset = center.row % 2 === 0 && center.row > 0 ? 0 : 1;
+  const radius = 4;
+  const positions = [] as any[];
+  const normalizedCenter = { col: 4, row: 4 };
+  const shifted = { col: center.col - normalizedCenter.col, row: center.row - normalizedCenter.row };
+  for (let _row = normalizedCenter.row - radius; _row <= normalizedCenter.row + radius; _row++) {
+    const basicCount = 9;
+    const decrease = Math.abs(_row - radius);
+    const colsCount = basicCount - decrease;
+    let startOffset = _row % 2 === 0 ? (decrease > 0 ? Math.floor(decrease / 2) : 0) : Math.floor(decrease / 2);
+    if (addOffset > 0 && _row % 2 !== 0) {
+      if (center.row < 0 && center.row % 2 === 0) startOffset += 1;
+    }
+    for (
+      let _col = startOffset + normalizedCenter.col - radius;
+      _col < normalizedCenter.col - radius + colsCount + startOffset;
+      _col++
+    ) {
+      positions.push({
+        ...getUIPositionFromColRow(_col + shifted.col, _row + shifted.row, true),
+        z: 0.32,
+        color: _color,
+        col: _col + shifted.col,
+        row: _row + shifted.row,
+        startOffset: startOffset,
+      });
+    }
+  }
+
+  return positions;
 };
 
 export default HexBuildGrid;
