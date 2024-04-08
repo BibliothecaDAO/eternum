156613fc29fd32d69ed87ba9c508918075888b72
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-04-08 12:51:27 +1000

    module work

--- client/src/ui/modules/resources/Resources.tsx
+++ client/src/ui/modules/resources/Resources.tsx
@@ -2,16 +2,50 @@ import useUIStore from "../../../hooks/store/useUIStore";
 import { OSWindow } from "../../components/navigation/OSWindow";
 import { leaderboard, resources } from "../../components/navigation/Config";
 import { EntityResourceTable } from "../../components/resources/EntityResourceTable";
+import { Tabs } from "@/ui/elements/tab";
+import { useMemo, useState } from "react";
 
 export const Resources = () => {
   const { togglePopup } = useUIStore();
-
+  const [selectedTab, setSelectedTab] = useState(0);
   const isOpen = useUIStore((state) => state.isPopupOpen(resources));
-
+  const tabs = useMemo(
+    () => [
+      {
+        key: "all",
+        label: (
+          <div className="flex relative group flex-col items-center">
+            <div>Balance</div>
+          </div>
+        ),
+        component: <EntityResourceTable />,
+      },
+      {
+        key: "mine",
+        label: (
+          <div className="flex relative group flex-col items-center">
+            <div>Production</div>
+          </div>
+        ),
+        component: <div>Production</div>,
+      },
+    ],
+    [selectedTab],
+  );
   return (
     <OSWindow onClick={() => togglePopup(resources)} show={isOpen} title={resources}>
-      {/* COMPONENTS GO HERE */}
-      <EntityResourceTable />
+      <Tabs selectedIndex={selectedTab} onChange={(index: any) => setSelectedTab(index)} className="h-full">
+        <Tabs.List>
+          {tabs.map((tab, index) => (
+            <Tabs.Tab key={index}>{tab.label}</Tabs.Tab>
+          ))}
+        </Tabs.List>
+        <Tabs.Panels className="overflow-hidden">
+          {tabs.map((tab, index) => (
+            <Tabs.Panel key={index}>{tab.component}</Tabs.Panel>
+          ))}
+        </Tabs.Panels>
+      </Tabs>
     </OSWindow>
   );
 };
