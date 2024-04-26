15ed814aa7dc2e85137f4f48e0cacfe54991b7a3
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-04-26 13:21:12 +1000

    fix env

--- client/dojoConfig.ts
+++ client/dojoConfig.ts
@@ -8,7 +8,7 @@ const {
   VITE_PUBLIC_MASTER_ADDRESS,
   VITE_PUBLIC_MASTER_PRIVATE_KEY,
   VITE_PUBLIC_ACCOUNT_CLASS_HASH,
-  VITE_DEV,
+  VITE_PUBLIC_DEV,
   VITE_PUBLIC_FEE_TOKEN_ADDRESS,
 } = import.meta.env;
 
@@ -20,5 +20,5 @@ export const dojoConfig = createDojoConfig({
   accountClassHash:
     VITE_PUBLIC_ACCOUNT_CLASS_HASH || "0x05400e90f7e0ae78bd02c77cd75527280470e2fe19c54970dd79dc37a9d3645c",
   feeTokenAddress: VITE_PUBLIC_FEE_TOKEN_ADDRESS || "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-  manifest: VITE_DEV ? manifest : productionManifest,
+  manifest: VITE_PUBLIC_DEV ? manifest : productionManifest,
 });
