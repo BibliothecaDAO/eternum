3f67b96e1d4102ff8ab5eaded9de8ea65e8df8e0
Await-0x <146035814+Await-0x@users.noreply.github.com>
2024-04-26 22:23:52 +0200

    correct counter rotation on letters

--- client/src/ui/components/worldmap/Compass.tsx
+++ client/src/ui/components/worldmap/Compass.tsx
@@ -38,10 +38,10 @@ export const Compass = () => {
         <div className="absolute h-[15px] border-r-[1px] p-r-[2px] border-gold opacity-50"></div>
         <div className="absolute w-[15px] border-t-[1px] p-t-[2px] border-gold opacity-50"></div>
 
-        <div className="absolute top-[-2px] text-xxs" style={{ transform: `rotate(-${direction}deg)` }}>N</div>
-        <div className="absolute right-[2px] text-xxs opacity-80" style={{ transform: `rotate(-${direction}deg)` }}>E</div>
-        <div className="absolute bottom-[-3px] text-xxs opacity-80" style={{ transform: `rotate(-${direction}deg)` }}>S</div>
-        <div className="absolute left-0 text-xxs opacity-80" style={{ transform: `rotate(-${direction}deg)` }}>W</div>
+        <div className="absolute top-[-2px] text-xxs" style={{ transform: `rotate(${direction * -1}deg)` }}>N</div>
+        <div className="absolute right-[2px] text-xxs opacity-80" style={{ transform: `rotate(${direction * -1}deg)` }}>E</div>
+        <div className="absolute bottom-[-3px] text-xxs opacity-80" style={{ transform: `rotate(${direction * -1}deg)` }}>S</div>
+        <div className="absolute left-0 text-xxs opacity-80" style={{ transform: `rotate(${direction * -1}deg)` }}>W</div>
 
       </div>
     </div>
