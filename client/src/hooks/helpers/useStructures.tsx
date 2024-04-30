4c150dabaa86a402ef78d9292684ea979dd3fa15
aymericdelab <aymericdelabrousse@gmail.com>
2024-04-30 18:46:58 +0200

    fix build

--- client/src/hooks/helpers/useStructures.tsx
+++ client/src/hooks/helpers/useStructures.tsx
@@ -11,8 +11,8 @@ export const useStructures = () => {
   // to do: change that when more generalised structure component is added
 
   const hasStructures = (col: number, row: number) => {
-    const bankEntities = runQuery([HasValue(Position, { x: BigInt(col), y: BigInt(row) }), Has(Bank)]);
-    const realmEntities = runQuery([HasValue(Position, { x: BigInt(col), y: BigInt(row) }), Has(Realm)]);
+    const bankEntities = runQuery([HasValue(Position, { x: col, y: row }), Has(Bank)]);
+    const realmEntities = runQuery([HasValue(Position, { x: col, y: row }), Has(Realm)]);
     // add settlement
     // add hyperstructure
     return Array.from(bankEntities).length > 0 || Array.from(realmEntities).length > 0;
