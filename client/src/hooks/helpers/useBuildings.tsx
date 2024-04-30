d191a00dea1dfb3d2792fe8a6faa9a5dec8d6284
aymericdelab <aymericdelabrousse@gmail.com>
2024-04-30 15:39:21 +0200

    add optimisitc build

--- client/src/hooks/helpers/useBuildings.tsx
+++ client/src/hooks/helpers/useBuildings.tsx
@@ -1,11 +1,15 @@
 import { BuildingType } from "@bibliothecadao/eternum";
 import { useDojo } from "../context/DojoContext";
 import { CairoOption, CairoOptionVariant } from "starknet";
+import { uuid } from "@latticexyz/utils";
+import { getEntityIdFromKeys } from "@dojoengine/utils";
+import { getComponentValue } from "@dojoengine/recs";
 
 export function useBuildings() {
   const {
     account: { account },
     setup: {
+      components: { Building, Position },
       systemCalls: { create_building, destroy_building },
     },
   } = useDojo();
@@ -17,6 +21,25 @@ export function useBuildings() {
     buildingType: BuildingType,
     resourceType?: number,
   ) => {
+    // add optimisitc rendering
+    const realmPosition = getComponentValue(Position, getEntityIdFromKeys([realmEntityId]));
+    if (!realmPosition) return;
+    const { x: outercol, y: outerrow } = realmPosition;
+    const category = BuildingType[buildingType];
+    const entity = getEntityIdFromKeys([outercol, outerrow, col, row].map((v) => BigInt(v)));
+    const overrideId = uuid();
+    Building.addOverride(overrideId, {
+      entity,
+      value: {
+        outer_col: BigInt(outercol),
+        outer_row: BigInt(outerrow),
+        inner_col: BigInt(col),
+        inner_row: BigInt(row),
+        category,
+        produced_resource_type: resourceType ? resourceType : 0,
+      },
+    });
+
     await create_building({
       signer: account,
       entity_id: realmEntityId as bigint,
@@ -29,10 +52,13 @@ export function useBuildings() {
         buildingType == BuildingType.Resource && resourceType
           ? new CairoOption<Number>(CairoOptionVariant.Some, resourceType)
           : new CairoOption<Number>(CairoOptionVariant.None, 0),
+    }).catch(() => {
+      Building.removeOverride(overrideId);
     });
   };
 
   const destroyBuilding = async (realmEntityId: bigint, col: number, row: number) => {
+    // add optimisitc rendering
     await destroy_building({
       signer: account,
       entity_id: realmEntityId as bigint,
