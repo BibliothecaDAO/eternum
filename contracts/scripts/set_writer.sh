2284d2c7e5a9c02b6d3e05732ba2b13d8090e321
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-05-01 16:08:02 +1000

    fixes

--- contracts/scripts/set_writer.sh
+++ contracts/scripts/set_writer.sh
@@ -49,7 +49,7 @@ for system in $(echo $system_models_json | jq -r 'keys[]'); do
         system_var="${system}"
         contract_address="${!system_var}"
         # make the system a writer of the component
-        if [[ "$1" == "prod" ]]; then
+        if [[ "$mode" != "prod" && "$mode" != "dev" ]]; then
             commands+=("sozo --profile prod auth grant writer $model,$contract_address")
         else
             commands+=("sozo auth grant writer $model,$contract_address")
