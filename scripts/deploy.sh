ee3f06f0a2c1408eac3e39db6ea407766d33d185
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-05-07 08:43:26 +1000

    builds

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,13 @@ echo "Build contracts..."
 sozo --profile prod build
 
 echo "Deploying world to Realms L3..."
-sozo --profile prod migrate apply --name eternum8
+sozo --profile prod migrate apply --name eternum10
 
 echo "Deleting previous indexer..."
 slot deployments delete eternum torii
 
 echo "Setting up remote indexer on slot..."
-slot deployments create eternum-9 torii --version 0.6.1-alpha.4 --world $SOZO_WORLD --rpc https://api.cartridge.gg/x/realms/katana/ --start-block 0
+slot deployments create eternum-10 torii --version 0.6.1-alpha.4 --world $SOZO_WORLD --rpc https://api.cartridge.gg/x/realms/katana/ --start-block 0
 
 echo "Setting up config..."
 ./scripts/set_config.sh --interval 1 --mode prod
