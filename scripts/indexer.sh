41458626c8cf182c9160a1d7640dc024b5bf803c
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-05-12 19:25:13 +1000

    in script

--- scripts/indexer.sh
+++ scripts/indexer.sh
@@ -9,11 +9,11 @@ sozo build
 echo "----- Migrating World -----"
 sozo migrate apply --name eternum
 
+bun --env-file=./client/.env.development ./config/index.ts
 
 echo "----- Auth and World Contracts: Set 0.1s ----- "
 source scripts/env_variables.sh dev
 ./scripts/set_config.sh --interval 0.1 --mode dev
-
 
 echo "-----  Started indexer ----- "
 torii --world 0x177a3f3d912cf4b55f0f74eccf3b7def7c6144efeba033e9f21d9cdb0230c64
