0f87f40d4e95cbdffd502e8a433e75f21470310e
aymericdelab <aymericdelabrousse@gmail.com>
2024-05-17 11:05:23 +0200

    clean

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,7 +16,7 @@ echo "Setting up remote indexer on slot..."
 slot deployments create eternum-11 torii --version 0.6.1-alpha.4 --world $SOZO_WORLD --rpc https://api.cartridge.gg/x/realms/katana/ --start-block 0
 
 echo "Setting up config..."
-./scripts/set_config.sh --interval 1 --mode prod
+./scripts/set_writer.sh --interval 1  --mode prod 
 
 
 echo "Setting up bank..."
--- scripts/indexer.sh
+++ scripts/indexer.sh
@@ -13,7 +13,7 @@ bun --env-file=../client/.env.development ../config/index.ts
 
 echo "----- Auth and World Contracts: Set 0.1s ----- "
 source scripts/env_variables.sh dev
-./scripts/set_config.sh --interval 0.1 --mode dev
+./scripts/set_writer.sh --interval 0.1  --mode dev
 
 echo "-----  Started indexer ----- "
 torii --world 0x177a3f3d912cf4b55f0f74eccf3b7def7c6144efeba033e9f21d9cdb0230c64
