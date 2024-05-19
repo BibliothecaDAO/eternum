308f3a6310fbf4ca5e57545452b7daccb905b068
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-05-20 09:33:51 +1000

    add realm select

--- client/src/ui/components/trading/MarketModal.tsx
+++ client/src/ui/components/trading/MarketModal.tsx
@@ -4,12 +4,34 @@ import { useMemo, useState } from "react";
 import Button from "@/ui/elements/Button";
 import TextInput from "@/ui/elements/TextInput";
 import { MarketOrderPanel, MarketResource } from "./MarketOrderPanel";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/ui/elements/Select";
+import { useEntities } from "@/hooks/helpers/useEntities";
+import useRealmStore from "@/hooks/store/useRealmStore";
 
 export const MarketModal = () => {
+  const { playerRealms } = useEntities();
+
+  const { realmEntityId, setRealmEntityId } = useRealmStore();
+
   return (
     <ModalContainer>
       <div className="container border mx-auto  grid grid-cols-12 my-8">
-        <div className="col-span-12 h-16 border"></div>
+        <div className="col-span-12 border p-2">
+          <div className="self-center">
+            <Select value={realmEntityId.toString()} onValueChange={(trait) => setRealmEntityId(BigInt(trait))}>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select Realm" />
+              </SelectTrigger>
+              <SelectContent className="bg-brown text-gold">
+                {playerRealms().map((realm, index) => (
+                  <SelectItem key={index} value={realm.entity_id?.toString() || ""}>
+                    {realm.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
 
         <div className="col-span-2 border ">
           <MarketResourceSidebar search={""} />
