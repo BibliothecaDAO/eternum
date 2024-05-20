24e8d867d3480d90aff848c2eb620a98fd896cd8
0x1337 <za6ah9t@icloud.com>
2024-05-20 20:54:22 +0700

    fix: hightlight refs

--- client/src/ui/components/worldmap/hexagon/HighlightedHexes.tsx
+++ client/src/ui/components/worldmap/hexagon/HighlightedHexes.tsx
@@ -10,19 +10,21 @@ const HighlightedHexes = () => {
   const radialSegments = 10; // Adjust for smoother or sharper corners
   const tubularSegments = 64; // Adjust for a smoother or more faceted tube
   const hexagonPath = createHexagonPath(HEX_RADIUS);
-  const hexagonGeometry = new THREE.RingGeometry(2, 1.5, 6, 1);
+  const hexagonGeometry = new THREE.RingGeometry(2, 1.5, 8, 1);
 
   const highlightPositions = useUIStore((state) => state.highlightPositions);
 
-  const meshRef = useRef<any>();
+  const meshRefs = useRef<any[]>([]);
 
   useFrame(({ clock }) => {
     const elapsedTime = clock.getElapsedTime();
     const pulseFactor = Math.sin(elapsedTime * Math.PI) * 0.2 + 0.5;
-    if (meshRef.current?.material) {
-      meshRef.current.material.emissiveIntensity = pulseFactor;
-      meshRef.current.rotation.z = elapsedTime * Math.PI;
-    }
+    meshRefs.current.forEach((mesh) => {
+      if (mesh?.material) {
+        mesh.material.emissiveIntensity = pulseFactor;
+        mesh.rotation.z = elapsedTime * Math.PI;
+      }
+    });
   });
 
   return (
@@ -31,12 +33,12 @@ const HighlightedHexes = () => {
         return (
           <mesh
             key={index}
-            ref={meshRef}
+            ref={(el) => (meshRefs.current[index] = el)}
             geometry={hexagonGeometry}
             rotation={[Math.PI / 2, 0, Math.PI / 2]}
             position={[highlightPosition[0], 0.3, highlightPosition[1]]}
           >
-            <meshPhongMaterial color={highlightColor} emissive={"green"} />
+            <meshStandardMaterial color={highlightColor} emissive={"green"} />
           </mesh>
         );
       })}
