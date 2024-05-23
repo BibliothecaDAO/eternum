62e69048ef8def2e25860fd712e6816d34715ffa
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-05-23 16:41:12 +1000

    release

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,18 @@ echo "Build contracts..."
 sozo --profile prod build
 
 echo "Deploying world to Realms L3..."
-sozo --profile prod migrate apply --name eternum10
+sozo --profile prod migrate apply --name eternum11
 
 echo "Deleting previous indexer..."
 slot deployments delete eternum torii
 
 echo "Setting up remote indexer on slot..."
-slot deployments create eternum-11 torii --version 0.6.1-alpha.4 --world $SOZO_WORLD --rpc https://api.cartridge.gg/x/realms/katana/ --start-block 0
+slot deployments create eternum-12 torii --version 0.6.1-alpha.4 --world 0x2c301e106e9f02e00fd1c3fbbecac3b23f0147757f694cd6d3eed70f1ca338d --rpc https://api.cartridge.gg/x/realms/katana/ --start-block 0
+
+bun --env-file=../client/.env.production ../config/index.ts
+
+echo "----- Auth and World Contracts: Set 0.1s ----- "
+source scripts/env_variables.sh dev
 
 echo "Setting up config..."
 ./scripts/set_writer.sh --interval 1  --mode prod 
