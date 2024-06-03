74cc2161ae5639c38a46bea0b2ace7b9bf53aece
tedison <timothyedison@hotmail.com>
2024-06-03 13:27:22 +0200

    Fix undefined protector army stamina

--- client/src/ui/elements/StaminaResource.tsx
+++ client/src/ui/elements/StaminaResource.tsx
@@ -2,19 +2,18 @@ import { useStamina } from "@/hooks/helpers/useStamina";
 import { EternumGlobalConfig } from "@bibliothecadao/eternum";
 import clsx from "clsx";
 
-export const StaminaResource = ({ entityId, className }: { entityId: bigint; className?: string }) => {
+export const StaminaResource = ({ entityId, className }: { entityId: bigint | undefined; className?: string }) => {
   const { useStaminaByEntityId, getMaxStaminaByEntityId } = useStamina();
-  const stamina = useStaminaByEntityId({ travelingEntityId: entityId });
+  const stamina = useStaminaByEntityId({ travelingEntityId: entityId || 0n });
 
-  const staminaColor =
-    stamina !== undefined && stamina.amount < EternumGlobalConfig.stamina.travelCost ? "text-red/90" : "text-green/90";
+  const staminaColor = stamina?.amount || 0n < EternumGlobalConfig.stamina.travelCost ? "text-red/90" : "text-green/90";
 
   return (
     <div className={`flex flex-row p-1 text-xs ${className}`}>
       <div className="text-lg p-1 pr-3">⚡️</div>
       <div className={clsx(staminaColor, "flex", "flex-col")}>
         <div>
-          {stamina.amount} / {getMaxStaminaByEntityId(entityId)}
+          {stamina?.amount || 0} / {getMaxStaminaByEntityId(entityId || 0n)}
         </div>
         <div>Stamina</div>
       </div>
