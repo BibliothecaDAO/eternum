c8fd681e8e13bc220839c8ec95933f4404b3dd2a
Constantin Wastchenko <80072466+cwastche@users.noreply.github.com>
2024-06-11 20:20:25 +0200

    Fixes quest number behind container (#889)

--- client/src/ui/containers/BottomMiddleContainer.tsx
+++ client/src/ui/containers/BottomMiddleContainer.tsx
@@ -1,5 +1,5 @@
 const BottomMiddleContainer = ({ children }: { children: React.ReactNode }) => {
-  return <div className="absolute w-screen bottom-3 flex ">{children}</div>;
+  return <div className="absolute w-screen bottom-3 flex">{children}</div>;
 };
 
 export default BottomMiddleContainer;
