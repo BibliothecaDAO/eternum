2e4eb6c9465b66a063be224b1c9f4e62ce8e34c8
aymericdelab <aymericdelabrousse@gmail.com>
2024-06-12 17:27:44 +0200

    add battle model

---
+++ client/src/ui/components/models/buildings/worldmap/Battles.tsx
@@ -0,0 +1,24 @@
+import { useBattles } from "@/hooks/helpers/useBattles";
+import { getUIPositionFromColRow } from "@/ui/utils/utils";
+import { useGLTF } from "@react-three/drei";
+import { useMemo } from "react";
+
+export const Battles = () => {
+  const { allBattles } = useBattles();
+  const battles = allBattles();
+
+  battles.map((battle, index) => {
+    if (!battle?.x || !battle?.y) return null;
+    const { x, y } = getUIPositionFromColRow(battle.x, battle.y, false);
+    return <BattleModel key={index} position={[x, 0.31, -y]} onClick={() => {}} />;
+  });
+};
+
+const BattleModel = ({ position, onClick }: { position: any; onClick: () => void }) => {
+  const model = useGLTF("/models/buildings/barracks.glb");
+  const clone = useMemo(() => {
+    return model.scene.clone();
+  }, [model]);
+
+  return <primitive scale={3} object={clone} position={position} onClick={onClick} />;
+};
