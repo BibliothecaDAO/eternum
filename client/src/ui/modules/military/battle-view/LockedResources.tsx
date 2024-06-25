3848c8bc813a54ae229e3435dc9a47c679820137
tedison <timothyedison@hotmail.com>
2024-06-25 15:24:13 +0200

    Display message in case of empty battle chest

--- client/src/ui/modules/military/battle-view/LockedResources.tsx
+++ client/src/ui/modules/military/battle-view/LockedResources.tsx
@@ -1,4 +1,6 @@
+import { useResources } from "@/hooks/helpers/useResources";
 import { InventoryResources } from "@/ui/components/resources/InventoryResources";
+import React from "react";
 
 export const LockedResources = ({
   attackersResourcesEscrowEntityId,
@@ -7,17 +9,28 @@ export const LockedResources = ({
   attackersResourcesEscrowEntityId: bigint;
   defendersResourcesEscrowEntityId: bigint;
 }) => {
+  const { getResourcesFromBalance } = useResources();
+
+  const attackersResources = getResourcesFromBalance(attackersResourcesEscrowEntityId);
+  const defendersResources = getResourcesFromBalance(attackersResourcesEscrowEntityId);
+
   return (
     <div className="col-span-2 flex justify-center flex-wrap ornate-borders-bottom-y p-2 bg-[#1b1a1a] bg-map">
       <div className="text-center w-full text-gold">Battle Chest</div>
-      <div className="grid overflow-auto grid-cols-12 gap-2">
-        <div className="border-r border-gold/50  col-span-6">
-          <InventoryResources entityId={attackersResourcesEscrowEntityId} max={4} />
+      {attackersResources.length > 0 && defendersResources.length > 0 ? (
+        <div className="grid overflow-auto grid-cols-12 gap-2">
+          <React.Fragment>
+            <div className="border-r border-gold/50  col-span-6">
+              <InventoryResources entityId={attackersResourcesEscrowEntityId} max={4} />
+            </div>
+            <div className="col-span-6">
+              <InventoryResources entityId={defendersResourcesEscrowEntityId} max={4} />
+            </div>
+          </React.Fragment>
         </div>
-        <div className="col-span-6">
-          <InventoryResources entityId={defendersResourcesEscrowEntityId} max={4} />
-        </div>
-      </div>
+      ) : (
+        <div className="text-xl text-gold w-full px-2 text-center">Empty</div>
+      )}
     </div>
   );
 };
