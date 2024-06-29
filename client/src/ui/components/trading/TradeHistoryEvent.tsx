4070c6ce5509f7d30cb7143513e7fda7c98f6bad
tedison <timothyedison@hotmail.com>
2024-06-29 10:50:09 +0200

    Add colors to trade history

--- client/src/ui/components/trading/TradeHistoryEvent.tsx
+++ client/src/ui/components/trading/TradeHistoryEvent.tsx
@@ -3,8 +3,9 @@ import { useEntities } from "@/hooks/helpers/useEntities";
 import { useResourceBalance } from "@/hooks/helpers/useResources";
 import useBlockchainStore from "@/hooks/store/useBlockchainStore";
 import { ResourceIcon } from "@/ui/elements/ResourceIcon";
+import { currencyIntlFormat } from "@/ui/utils/utils";
 import { EternumGlobalConfig, ResourcesIds } from "@bibliothecadao/eternum";
-import { getComponentValue } from "@dojoengine/recs";
+import { ComponentValue, getComponentValue, Type } from "@dojoengine/recs";
 import { getEntityIdFromKeys } from "@dojoengine/utils";
 import { TradeEvent } from "./MarketTradingHistory";
 
@@ -16,6 +17,14 @@ export enum EventType {
   BOUGHT = "Took an order",
 }
 
+const colors = {
+  [EventType.ORDER_CREATED]: "text-gold",
+  [EventType.ORDER_ACCEPTED]: "text-green",
+  [EventType.ORDER_CANCELLED]: "text-danger",
+  [EventType.ORDER_EXPIRED]: "text-danger",
+  [EventType.BOUGHT]: "text-green",
+};
+
 export enum TradeStatus {
   OPEN = 0,
   ACCEPTED = 1,
@@ -24,11 +33,12 @@ export enum TradeStatus {
 
 export const TradeHistoryRowHeader = () => {
   return (
-    <div className="grid grid-cols-5 gap-1 flex-grow overflow-y-auto mb-4">
+    <div className="grid grid-cols-6 gap-1 flex-grow overflow-y-auto mb-4">
       <div className="uppercase text-xs font-bold">Time</div>
       <div className="uppercase text-xs font-bold">Status</div>
       <div className="uppercase text-xs font-bold">Taker</div>
       <div className="uppercase text-xs font-bold">Trade</div>
+      <div className="uppercase text-xs font-bold">Price</div>
       <div className="uppercase text-xs font-bold">Expiration</div>
     </div>
   );
@@ -67,21 +77,66 @@ export const TradeHistoryEvent = ({ trade }: { trade: TradeEvent }) => {
   const expirationDate =
     trade.type === EventType.ORDER_CREATED ? new Date(tradeComponent!.expires_at * 1000) : undefined;
 
+  const price = getPrice(resourceGiven[0], resourceTaken[0]);
+
   return (
-    <div className="grid grid-cols-5 gap-1 flex-grow overflow-y-auto h-14 mb-4 p-1 clip-angled-sm">
-      <div className="text-xs my-auto">{`${trade.event.eventTime.toLocaleDateString()} ${trade.event.eventTime.toLocaleTimeString()}`}</div>
-      <div className="text-sm my-auto">{eventType}</div>
-      <div className="text-sm my-auto">{taker}</div>
-      <div className="text-sm my-auto flex flex-row">
+    <div className="grid grid-cols-6 gap-1 flex-grow overflow-y-auto h-14 mb-4 p-1 clip-angled-sm">
+      <div
+        className={`text-xs my-auto`}
+      >{`${trade.event.eventTime.toLocaleDateString()} ${trade.event.eventTime.toLocaleTimeString()}`}</div>
+      <div className={`text-sm my-auto ${colors[eventType]}`}>{eventType}</div>
+      <div className={`text-sm my-auto`}>{taker}</div>
+      <div className="text-sm my-auto flex flex-row font-bold">
         {resourceGiven && <ResourceIcon resource={ResourcesIds[Number(resourceGiven[0]!.resource_type)]} size={"sm"} />}{" "}
-        <div>{`${Number(resourceGiven[0]!.resource_amount) / EternumGlobalConfig.resources.resourcePrecision} for ${
-          Number(resourceTaken[0]!.resource_amount) / EternumGlobalConfig.resources.resourcePrecision
-        }`}</div>
+        <div>{`${currencyIntlFormat(
+          Number(resourceGiven[0]!.resource_amount) / EternumGlobalConfig.resources.resourcePrecision,
+          2,
+        )} for ${currencyIntlFormat(
+          Number(resourceTaken[0]!.resource_amount) / EternumGlobalConfig.resources.resourcePrecision,
+          2,
+        )}`}</div>
         {resourceTaken && <ResourceIcon resource={ResourcesIds[Number(resourceTaken[0]!.resource_type)]} size={"sm"} />}{" "}
+      </div>
+      <div className="text-sm my-auto flex flex-row font-bold">
+        {currencyIntlFormat(price, 2)}
+        <ResourceIcon resource={ResourcesIds[Number(resourceTaken[0]!.resource_type)]} size={"sm"} />
+        per/
+        <ResourceIcon resource={ResourcesIds[Number(resourceGiven[0]!.resource_type)]} size={"sm"} />
       </div>
       <div className="text-sm my-auto">
         {expirationDate && `${expirationDate.toLocaleDateString()} ${expirationDate.toLocaleTimeString()}`}
       </div>
     </div>
   );
+};
+
+const getPrice = (
+  resourceA:
+    | ComponentValue<
+        {
+          entity_id: Type.BigInt;
+          index: Type.Number;
+          resource_type: Type.Number;
+          resource_amount: Type.BigInt;
+        },
+        unknown
+      >
+    | undefined,
+  resourceB:
+    | ComponentValue<
+        {
+          entity_id: Type.BigInt;
+          index: Type.Number;
+          resource_type: Type.Number;
+          resource_amount: Type.BigInt;
+        },
+        unknown
+      >
+    | undefined,
+): Number => {
+  if (resourceA!.resource_type === ResourcesIds.Lords) {
+    return Number(resourceA!.resource_amount) / Number(resourceB!.resource_amount);
+  } else {
+    return Number(resourceB!.resource_amount) / Number(resourceA!.resource_amount);
+  }
 };
