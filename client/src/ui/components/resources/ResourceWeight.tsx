658f65125e1a1c26fbd0a1ca52a1ee602874abd3
aymericdelab <aymericdelabrousse@gmail.com>
2024-07-05 18:16:29 +0200

    revert travel info changes

--- client/src/ui/components/resources/ResourceWeight.tsx
+++ client/src/ui/components/resources/ResourceWeight.tsx
@@ -1,61 +1,53 @@
+import { useResourceBalance } from "@/hooks/helpers/useResources";
 import { divideByPrecision, multiplyByPrecision } from "@/ui/utils/utils";
 import { EternumGlobalConfig, Resource, ResourcesIds, WEIGHTS } from "@bibliothecadao/eternum";
-import { getTotalResourceWeight } from "../cityview/realm/trade/utils";
 import { useEffect, useState } from "react";
-import { useResourceBalance } from "@/hooks/helpers/useResources";
+import { getTotalResourceWeight } from "../cityview/realm/trade/utils";
 
 export const TravelInfo = ({
   entityId,
   resources,
   travelTime,
-  isPickup,
   setCanCarry,
 }: {
   entityId: bigint;
   resources: Resource[];
   travelTime?: number;
-  isPickup?: boolean;
   setCanCarry?: (canContinue: boolean) => void;
 }) => {
   const [resourceWeight, setResourceWeight] = useState(0);
   const [donkeyBalance, setDonkeyBalance] = useState(0);
-  const [sendingDonkeys, setSendingDonkeys] = useState(0);
   const neededDonkeys = Math.ceil(divideByPrecision(resourceWeight) / EternumGlobalConfig.carryCapacity.donkey);
 
   const { getBalance } = useResourceBalance();
 
   useEffect(() => {
-    const updateResourceWeight = () => {
-      const totalWeight = getTotalResourceWeight(resources);
-      const multipliedWeight = multiplyByPrecision(totalWeight);
-      setResourceWeight(multipliedWeight);
+    const totalWeight = getTotalResourceWeight(resources);
+    const multipliedWeight = multiplyByPrecision(totalWeight);
+    setResourceWeight(multipliedWeight);
 
-      const { balance } = getBalance(entityId, ResourcesIds.Donkey);
-      const currentDonkeyAmount = isPickup
-        ? 0
-        : resources.find((r) => r.resourceId === ResourcesIds.Donkey)?.amount || 0;
-      const calculatedDonkeyBalance = divideByPrecision(balance) - currentDonkeyAmount;
-      setDonkeyBalance(calculatedDonkeyBalance);
-      setSendingDonkeys(currentDonkeyAmount);
+    const { balance } = getBalance(entityId, ResourcesIds.Donkey);
+    const currentDonkeyAmount = resources.find((r) => r.resourceId === ResourcesIds.Donkey)?.amount || 0;
+    const calculatedDonkeyBalance = divideByPrecision(balance) - currentDonkeyAmount;
+    setDonkeyBalance(calculatedDonkeyBalance);
 
-      if (setCanCarry) {
-        setCanCarry(calculatedDonkeyBalance >= neededDonkeys);
-      }
-    };
-
-    updateResourceWeight();
+    if (setCanCarry) {
+      setCanCarry(calculatedDonkeyBalance >= neededDonkeys);
+    }
   }, [resources, entityId, resourceWeight, donkeyBalance, setCanCarry]);
 
   return (
     <>
       <table className="min-w-full divide-y divide-gray-200 text-sm mt-2 text-center font-bold ">
         <tbody className=" divide-y divide-gray-200 ">
-          <tr>
-            <td className="px-6 py-1 whitespace-nowrap  font-bold text-right">Travel Time</td>
-            <td className="px-6 py-1 whitespace-nowrap text-gold text-left">
-              {`${Math.floor(travelTime! / 60)} hrs ${travelTime! % 60} mins`}
-            </td>
-          </tr>
+          {travelTime && (
+            <tr>
+              <td className="px-6 py-1 whitespace-nowrap  font-bold text-right">Travel Time</td>
+              <td className="px-6 py-1 whitespace-nowrap text-gold text-left">
+                {`${Math.floor(travelTime / 60)} hrs ${travelTime % 60} mins`}
+              </td>
+            </tr>
+          )}
 
           <tr>
             <td className="px-6 py-1 whitespace-nowrap  font-bold text-right">Total Transfer Weight</td>
@@ -70,7 +62,7 @@ export const TravelInfo = ({
                 neededDonkeys > donkeyBalance ? "text-red" : "text-green"
               }`}
             >
-              {neededDonkeys} + {sendingDonkeys} [{donkeyBalance}]{" "}
+              {neededDonkeys} [{donkeyBalance}]{" "}
             </td>
           </tr>
         </tbody>
