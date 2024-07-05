132bd201b41fffec6fa72418bd3106de15852218
aymericdelab <aymericdelabrousse@gmail.com>
2024-07-05 16:47:05 +0200

    styling

--- client/src/ui/components/bank/LiquidityTable.tsx
+++ client/src/ui/components/bank/LiquidityTable.tsx
@@ -13,8 +13,8 @@ export const LiquidityTable = ({ bank_entity_id, entity_id }: LiquidityTableProp
   }
 
   return (
-    <div className="p-4 bg-gold/10 ">
-      <table className="min-w-full ">
+    <div className="p-4 h-full bg-gold/10 overflow-x-auto">
+      <table className="min-w-full">
         <thead>
           <tr className="uppercase">
             <th className="text-left">Pair</th>
@@ -28,20 +28,24 @@ export const LiquidityTable = ({ bank_entity_id, entity_id }: LiquidityTableProp
             <th></th>
           </tr>
         </thead>
-        <tbody>
-          {Object.entries(RESOURCE_TIERS).map(([tier, resourceIds]) => {
-            if (tier === "lords") return;
-            return resourceIds.map((resourceId) => (
-              <LiquidityResourceRow
-                key={resourceId}
-                bankEntityId={bank_entity_id!}
-                entityId={entity_id}
-                resourceId={resourceId}
-              />
-            ));
-          })}
-        </tbody>
       </table>
+      <div className="overflow-y-auto">
+        <table className="min-w-full">
+          <tbody>
+            {Object.entries(RESOURCE_TIERS).map(([tier, resourceIds]) => {
+              if (tier === "lords") return;
+              return resourceIds.map((resourceId) => (
+                <LiquidityResourceRow
+                  key={resourceId}
+                  bankEntityId={bank_entity_id!}
+                  entityId={entity_id}
+                  resourceId={resourceId}
+                />
+              ));
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
