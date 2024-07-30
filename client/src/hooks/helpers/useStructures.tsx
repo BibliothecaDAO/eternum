3f966919fa3ada9d14dfcd6ddab4418c8fa9db1b
cwastche <wastchenko.constantin@gmail.com>
2024-07-30 16:19:49 +0200

    prettier

--- client/src/hooks/helpers/useStructures.tsx
+++ client/src/hooks/helpers/useStructures.tsx
@@ -149,10 +149,10 @@ export const getStructureAtPosition = ({ x, y }: Position): Structure | undefine
       structure.category === StructureType[StructureType.Realm]
         ? getRealmNameById(getComponentValue(Realm, structureEntityId)!.realm_id)
         : onChainName
-        ? shortString.decodeShortString(onChainName.name.toString())
-        : `${String(structure.category)
-            .replace(/([A-Z])/g, " $1")
-            .trim()} ${structure?.entity_id}`;
+          ? shortString.decodeShortString(onChainName.name.toString())
+          : `${String(structure.category)
+              .replace(/([A-Z])/g, " $1")
+              .trim()} ${structure?.entity_id}`;
 
     return {
       ...structure,
@@ -199,10 +199,10 @@ export const getStructureByPosition = () => {
       structure.category === StructureType[StructureType.Realm]
         ? getRealmNameById(getComponentValue(Realm, structureEntityId)!.realm_id)
         : onChainName
-        ? shortString.decodeShortString(onChainName.name.toString())
-        : `${String(structure.category)
-            .replace(/([A-Z])/g, " $1")
-            .trim()} ${structure?.entity_id}`;
+          ? shortString.decodeShortString(onChainName.name.toString())
+          : `${String(structure.category)
+              .replace(/([A-Z])/g, " $1")
+              .trim()} ${structure?.entity_id}`;
 
     return {
       ...structure,
@@ -248,10 +248,10 @@ export const getStructureByEntityId = (entityId: bigint) => {
       structure.category === StructureType[StructureType.Realm]
         ? getRealmNameById(getComponentValue(Realm, structureEntityId)!.realm_id)
         : onChainName
-        ? shortString.decodeShortString(onChainName.name.toString())
-        : `${String(structure.category)
-            .replace(/([A-Z])/g, " $1")
-            .trim()} ${structure?.entity_id}`;
+          ? shortString.decodeShortString(onChainName.name.toString())
+          : `${String(structure.category)
+              .replace(/([A-Z])/g, " $1")
+              .trim()} ${structure?.entity_id}`;
 
     return {
       ...structure,
