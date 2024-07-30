393ff24250bd9b20a60e1ddd46ff60f609a6c77b
aymericdelab <aymericdelabrousse@gmail.com>
2024-07-30 13:11:37 +0200

    fix action info bug

--- client/src/ui/components/worldmap/hexagon/ActionInfo.tsx
+++ client/src/ui/components/worldmap/hexagon/ActionInfo.tsx
@@ -12,45 +12,45 @@ import { useMemo } from "react";
 export const ActionInfo = () => {
   const { hoveredHex, selectedEntityId, travelPaths } = useThreeStore((state) => state.armyActions);
   const { getBalance } = getResourceBalance();
-  const { realmEntityId } = useRealmStore();
+  const realmEntityId = useRealmStore((state) => state.realmEntityId);
 
   const travelPath = useMemo(() => {
     if (hoveredHex) return travelPaths.get(`${hoveredHex.col + FELT_CENTER},${hoveredHex.row + FELT_CENTER}`);
   }, [hoveredHex, travelPaths]);
 
   const showTooltip = useMemo(() => {
-    return travelPath && travelPath.path.length >= 2 && selectedEntityId !== null;
+    return travelPath !== undefined && travelPath.path.length >= 2 && selectedEntityId !== null;
   }, [hoveredHex, travelPath, selectedEntityId]);
 
   const isExplored = travelPath?.isExplored || false;
 
   return (
     <>
-      {showTooltip && (
-        <BaseThreeTooltip position={Position.CLEAN} className="w-[250px]">
-          <Headline>{isExplored ? "Travel" : "Explore"}</Headline>
+      <BaseThreeTooltip position={Position.CLEAN} className="w-[250px]" visible={showTooltip}>
+        <Headline>{isExplored ? "Travel" : "Explore"}</Headline>
 
-          {!isExplored && (
-            <div>
-              <ResourceCost
-                amount={-EternumGlobalConfig.exploration.wheatBurn}
-                resourceId={ResourcesIds.Wheat}
-                balance={getBalance(realmEntityId, ResourcesIds.Wheat).balance}
-              />
-              <ResourceCost
-                amount={-EternumGlobalConfig.exploration.fishBurn}
-                resourceId={ResourcesIds.Fish}
-                balance={getBalance(realmEntityId, ResourcesIds.Fish).balance}
-              />
-            </div>
-          )}
+        {!isExplored && (
+          <div>
+            <ResourceCost
+              amount={-EternumGlobalConfig.exploration.wheatBurn}
+              resourceId={ResourcesIds.Wheat}
+              balance={getBalance(realmEntityId, ResourcesIds.Wheat).balance}
+            />
+            <ResourceCost
+              amount={-EternumGlobalConfig.exploration.fishBurn}
+              resourceId={ResourcesIds.Fish}
+              balance={getBalance(realmEntityId, ResourcesIds.Fish).balance}
+            />
+          </div>
+        )}
+        {showTooltip && (
           <StaminaResourceCost
             travelingEntityId={BigInt(selectedEntityId!)}
             isExplored={isExplored}
             travelLength={travelPath!.path.length - 1}
           />
-        </BaseThreeTooltip>
-      )}
+        )}
+      </BaseThreeTooltip>
     </>
   );
 };
