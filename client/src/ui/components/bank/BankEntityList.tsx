bad95df9cb6673084bfbd9f5665f5d695d2098ff
cwastche <wastchenko.constantin@gmail.com>
2024-07-31 09:58:30 +0200

    clean unused bank files

--- client/src/ui/components/bank/BankEntityList.tsx
+++
@@ -1,5 +0,0 @@
-import { EntityResourceTable } from "../resources/EntityResourceTable";
-
-export const BankEntityList = ({ entity }: any) => {
-  return <EntityResourceTable entityId={entity.id} />;
-};
