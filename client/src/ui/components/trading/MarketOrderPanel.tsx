ad39f746b61fa210f2387ca9ed582104de4434c0
cwastche <wastchenko.constantin@gmail.com>
2024-08-01 14:16:51 +0200

    fix sell order weight amount

--- client/src/ui/components/trading/MarketOrderPanel.tsx
+++ client/src/ui/components/trading/MarketOrderPanel.tsx
@@ -397,7 +397,7 @@ export const OrderCreation = ({
 
   const orderWeight = useMemo(() => {
     const totalWeight = getTotalResourceWeight([
-      { resourceId: isBuy ? resourceId : ResourcesIds.Lords, amount: resource },
+      { resourceId: isBuy ? resourceId : ResourcesIds.Lords, amount: isBuy ? resource : lords },
     ]);
     return multiplyByPrecision(totalWeight);
   }, [resource, lords]);
