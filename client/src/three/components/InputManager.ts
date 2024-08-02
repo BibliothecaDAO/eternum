01fb630d8f96457c3776a452af2719a55e9ffe1f
aymericdelab <aymericdelabrousse@gmail.com>
2024-08-02 16:03:26 +0200

    remove click when dragged

--- client/src/three/components/InputManager.ts
+++ client/src/three/components/InputManager.ts
@@ -1,13 +1,26 @@
 import * as THREE from "three";
 
-type ListenerTypes = "click" | "mousemove" | "contextmenu" | "dblclick";
+type ListenerTypes = "click" | "mousemove" | "contextmenu" | "dblclick" | "mouseup" | "mousedown";
 
 export class InputManager {
   private listeners: Array<{ event: ListenerTypes; handler: (e: MouseEvent) => void }> = [];
-  constructor(private raycaster: THREE.Raycaster, private mouse: THREE.Vector2, private camera: THREE.Camera) {}
+  private isDragged = false;
+  private mouseX = 0; // Add this flag
+  private mouseY = 0;
+
+  constructor(private raycaster: THREE.Raycaster, private mouse: THREE.Vector2, private camera: THREE.Camera) {
+    window.addEventListener("mousedown", this.handleMouseDown.bind(this));
+    window.addEventListener("mouseup", this.handleMouseUp.bind(this));
+  }
 
   addListener(event: ListenerTypes, callback: (raycaster: THREE.Raycaster) => void): void {
     const handler = (e: MouseEvent) => {
+      if (event === "click") {
+        if (this.isDragged) {
+          this.isDragged = false;
+          return;
+        }
+      }
       this.mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
       this.mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
       this.raycaster.setFromCamera(this.mouse, this.camera);
@@ -27,5 +40,14 @@ export class InputManager {
     for (const listener of this.listeners) {
       window.removeEventListener(listener.event, listener.handler);
     }
+  }
+
+  private handleMouseDown(e: MouseEvent): void {
+    this.mouseX = e.clientX;
+    this.mouseY = e.clientY;
+  }
+
+  private handleMouseUp(e: MouseEvent) {
+    if (this.mouseX - e.clientX > 10 || this.mouseY - e.clientY > 10) this.isDragged = true;
   }
 }
