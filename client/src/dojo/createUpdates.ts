8d41c0f2af8a0bcd4e0e9f9135085d47fa057ed6
tedison <timothyedison@hotmail.com>
2024-08-16 13:37:46 -0400

    fix events

--- client/src/dojo/createUpdates.ts
+++ client/src/dojo/createUpdates.ts
@@ -1,12 +1,8 @@
 import {
-  COMBAT_EVENT,
-  CREATE_ORDER_EVENT,
   HYPERSTRUCTURE_CO_OWNER_CHANGE,
   HYPERSTRUCTURE_FINISHED_EVENT,
   ID,
-  MAP_EXPLORED_EVENT,
   PILLAGE_EVENT,
-  TRAVEL_EVENT,
 } from "@bibliothecadao/eternum";
 import { numberToHex } from "../ui/utils/utils";
 import { createEventSubscription } from "./events/createEventSubscription";
@@ -15,16 +11,6 @@ export const createUpdates = async () => {
   const eventUpdates = {
     createHyperstructureFinishedEvents: async () => createEventSubscription([HYPERSTRUCTURE_FINISHED_EVENT]),
     createHyperstructureCoOwnerChangeEvents: async () => createEventSubscription([HYPERSTRUCTURE_CO_OWNER_CHANGE]),
-    createCombatEvents: async (entityId: ID) =>
-      createEventSubscription([COMBAT_EVENT, "*", numberToHex(Number(entityId)), "*"]),
-    createTravelEvents: async (x: number, y: number) =>
-      createEventSubscription([TRAVEL_EVENT, numberToHex(x), numberToHex(y)]),
-    createDirectOffersEvents: async (entityId: ID) =>
-      createEventSubscription([CREATE_ORDER_EVENT, numberToHex(Number(entityId)), "*"]),
-    createExploreMapEvents: async () => createEventSubscription([MAP_EXPLORED_EVENT], true, 1000),
-    createExploreEntityMapEvents: async (entityId: ID) =>
-      createEventSubscription([MAP_EXPLORED_EVENT, numberToHex(Number(entityId))]),
-    createTravelHexEvents: async () => createEventSubscription([TRAVEL_EVENT]),
     createPillageHistoryEvents: async (structureId: ID, attackerRealmEntityId: ID) =>
       createEventSubscription([PILLAGE_EVENT, numberToHex(structureId), numberToHex(attackerRealmEntityId)], true, 20),
   };
