de0b11c44a1e852f9da16b9045996189a4c2a826
0x1337 <za6ah9t@icloud.com>
2024-08-19 21:38:39 +0700

    feat: vignette

--- client/src/ui/containers/BottomMiddleContainer.tsx
+++ client/src/ui/containers/BottomMiddleContainer.tsx
@@ -1,5 +1,5 @@
 const BottomMiddleContainer = ({ children }: { children: React.ReactNode }) => {
-  return <div className="absolute w-screen bottom-1 flex pointer-events-none">{children}</div>;
+  return <div className="absolute w-screen bottom-1 flex pointer-events-none z-20">{children}</div>;
 };
 
 export default BottomMiddleContainer;
