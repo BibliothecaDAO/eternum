f8c0499895d887f23ddc5c800e4a8ab50bc4f1eb
tedison <timothyedison@hotmail.com>
2024-08-19 19:09:25 -0400

    prettier

--- client/src/ui/modules/entity-details/BuildingEntityDetails.tsx
+++ client/src/ui/modules/entity-details/BuildingEntityDetails.tsx
@@ -98,7 +98,7 @@ export const BuildingEntityDetails = () => {
             isPaused={isPaused}
             buildingId={buildingState.buildingType}
             entityId={buildingState.ownerEntityId}
-			hintModal
+            hintModal
           />
         )}
         {selectedBuildingHex && (
