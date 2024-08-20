cca2eb3c82990fa5bc3c991ed48ae867106839e7
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-08-20 09:49:03 -0400

    remove drizzle

--- bot/drizzle/0000_damp_cyclops.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"address" text,
-	"created_at" timestamp DEFAULT now(),
-	"discord" text,
-	"telegram" text
-);
--- bot/index.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@dojoengine/torii-client";
-
-export const toriiClient = await createClient([], {
-  rpcUrl: dojoConfig.rpcUrl,
-  toriiUrl: dojoConfig.toriiUrl,
-  relayUrl: "",
-  worldAddress: dojoConfig.manifest.world.address || "",
-});
