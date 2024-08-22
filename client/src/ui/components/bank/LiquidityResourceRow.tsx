94489f11a42a615300ff994b1eee7ba2187c90a1
raschel <38816784+aymericdelab@users.noreply.github.com>
2024-08-22 19:04:19 +0200

    Fix amm (#1400)
    * fix slippage bug
    
    * refactor
    
    * refactor
    
    * prettier
    
    * refactoring + amm styling
    
    * fix nan bug
    
    * fix entity select bug in bank
    
    * fix lords bug
    
    * fix fees display

--- client/src/ui/components/bank/LiquidityResourceRow.tsx
+++ client/src/ui/components/bank/LiquidityResourceRow.tsx
@@ -38,8 +38,7 @@ export const LiquidityResourceRow = ({ bankEntityId, entityId, resourceId }: Liq
   const marketManager = useMemo(
     () =>
       new MarketManager(
-        dojoContext.setup.components.Market,
-        dojoContext.setup.components.Liquidity,
+        dojoContext.setup,
         bankEntityId,
         ContractAddress(dojoContext.account.account.address),
         resourceId,
@@ -88,7 +87,7 @@ export const LiquidityResourceRow = ({ bankEntityId, entityId, resourceId }: Liq
         setIsLoading(false);
         setOpenConfirmation(false);
       });
-  }, [dojoContext, bankEntityId, resourceId, marketManager]);
+  }, [dojoContext, bankEntityId, entityId, resourceId, marketManager]);
 
   const renderConfirmationPopup = useMemo(() => {
     const travelResources = [
@@ -173,9 +172,16 @@ export const LiquidityResourceRow = ({ bankEntityId, entityId, resourceId }: Liq
         </div>
 
         <div>
-          <Button variant="outline" onClick={() => setOpenConfirmation(true)} isLoading={false} disabled={!canWithdraw}>
-            Withdraw
-          </Button>
+          <div className="flex items-center h-full">
+            <Button
+              variant="outline"
+              onClick={() => setOpenConfirmation(true)}
+              isLoading={false}
+              disabled={!canWithdraw}
+            >
+              Withdraw
+            </Button>
+          </div>
         </div>
       </div>
       {openConfirmation && renderConfirmationPopup}
