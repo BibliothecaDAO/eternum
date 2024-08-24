d50ef8c6271017715742b817501f024113d0e486
raschel <38816784+aymericdelab@users.noreply.github.com>
2024-08-25 00:09:20 +0200

    event colors (#1432)
    * refactoring
    
    * event colors

--- client/src/dojo/setup.ts
+++ client/src/dojo/setup.ts
@@ -16,7 +16,7 @@ export async function setup({ ...config }: DojoConfig) {
   // fetch all existing entities from torii
   const sync = await getSyncEntities(network.toriiClient, network.contractComponents as any, [], 1000);
 
-  const eventSync = getSyncEvents(network.toriiClient, network.contractComponents as any, undefined, []);
+  const eventSync = getSyncEvents(network.toriiClient, network.contractComponents.events as any, undefined, []);
 
   return {
     network,
