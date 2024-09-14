f6bee5ad0c720c2ff0e7868c9f08dd67afdb73af
tedison <timothyedison@hotmail.com>
2024-09-14 15:47:41 -0400

    cancel switch to alpha.9 as already done in another PR

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,13 +11,13 @@ slot deployments delete eternum-37 torii
 slot deployments delete eternum-37 katana
 
 echo "Deploying world to Realms L3..."
-slot deployments create --tier rare eternum-37 katana --version v1.0.0-alpha.9 --invoke-max-steps 25000000 --disable-fee true --block-time 1000
+slot deployments create --tier rare eternum-37 katana --version v1.0.0-alpha.11 --invoke-max-steps 25000000 --disable-fee true --block-time 1000
 
 echo "Migrating world..."
 sozo --profile prod migrate apply
 
 echo "Setting up remote indexer on slot..."
-slot deployments create --tier rare eternum-37 torii --version v1.0.0-alpha.9 --world 0x6918fe8c1ba16bdc83b9790cd9168d730aa98a22c65164578ef99af1c8cbc76 --rpc https://api.cartridge.gg/x/eternum-37/katana --start-block 0  --index-pending true
+slot deployments create --tier rare eternum-37 torii --version v1.0.0-alpha.11 --world 0x6918fe8c1ba16bdc83b9790cd9168d730aa98a22c65164578ef99af1c8cbc76 --rpc https://api.cartridge.gg/x/eternum-37/katana --start-block 0  --index-pending true
 
 echo "Setting up config..."
 ./scripts/set_writer.sh --interval 1  --mode prod 
