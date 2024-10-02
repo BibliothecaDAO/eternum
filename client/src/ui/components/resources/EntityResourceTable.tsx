58c32daf970aca6e1355832ca421c61d027fc490
tedison <76473430+edisontim@users.noreply.github.com>
2024-10-02 08:33:57 -0400

    feat: Stop resource production when max cap is reached (#1729)
    * Stop resource production when max cap is reached
    Closes #1725
    
    * remove console.log

--- client/src/ui/components/resources/EntityResourceTable.tsx
+++ client/src/ui/components/resources/EntityResourceTable.tsx
@@ -1,8 +1,27 @@
-import { ID, RESOURCE_TIERS } from "@bibliothecadao/eternum";
+import { useDojo } from "@/hooks/context/DojoContext";
+import { getEntityIdFromKeys, gramToKg } from "@/ui/utils/utils";
+import { BuildingType, CapacityConfigCategory, EternumGlobalConfig, ID, RESOURCE_TIERS } from "@bibliothecadao/eternum";
+import { useComponentValue } from "@dojoengine/react";
 import { useMemo } from "react";
 import { ResourceChip } from "./ResourceChip";
 
 export const EntityResourceTable = ({ entityId }: { entityId: ID | undefined }) => {
+  const dojo = useDojo();
+
+  const quantity =
+    useComponentValue(
+      dojo.setup.components.BuildingQuantityv2,
+      getEntityIdFromKeys([BigInt(entityId || 0), BigInt(BuildingType.Storehouse)]),
+    )?.value || 0;
+
+  const maxBalance = useMemo(() => {
+    return (
+      quantity * gramToKg(EternumGlobalConfig.carryCapacityGram[CapacityConfigCategory.Storehouse]) +
+      gramToKg(EternumGlobalConfig.carryCapacityGram[CapacityConfigCategory.Storehouse]) *
+        EternumGlobalConfig.resources.resourcePrecision
+    );
+  }, [quantity]);
+
   if (!entityId) {
     return <div>Entity not found</div>;
   }
@@ -10,7 +29,7 @@ export const EntityResourceTable = ({ entityId }: { entityId: ID | undefined }) 
   const resourceElements = useMemo(() => {
     return Object.entries(RESOURCE_TIERS).map(([tier, resourceIds]) => {
       const resources = resourceIds.map((resourceId: any) => {
-        return <ResourceChip key={resourceId} resourceId={resourceId} entityId={entityId} />;
+        return <ResourceChip key={resourceId} resourceId={resourceId} entityId={entityId} maxBalance={maxBalance} />;
       });
 
       return (
