52726a5e79a2532bb13b72441b9c7c710a0010df
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-10-08 19:23:31 +1100

    lordpedia fix and increase hyperstructure costs

--- client/src/ui/components/hints/HintModal.tsx
+++ client/src/ui/components/hints/HintModal.tsx
@@ -1,4 +1,3 @@
-import { Headline } from "@/ui/elements/Headline";
 import { useState } from "react";
 import { ModalContainer } from "../ModalContainer";
 import { Buildings } from "./Buildings";
@@ -86,46 +85,25 @@ export const HintModal = ({ initialActiveSection }: HintModalProps) => {
 
   return (
     <ModalContainer>
-      <div className="grid grid-cols-12 container mx-auto gap-4 bg-black/90 bg-hex-bg my-10 p-4  ">
-        <div className="col-span-12 text-center">
-          <h3>The Lordpedia</h3>
+      <div className="flex container mx-auto bg-black/90 bg-hex-bg my-10 rounded-xl border border-gold/40">
+        <div className="w-1/4 border-r border-gold/10 p-4">
+          <h3 className="text-center mb-4">The Lordpedia</h3>
+          <div className="space-y-1">
+            {sections.map((section) => (
+              <div
+                className={`p-2 px-4 hover:bg-gold/20 border border-gold/10 shadow-xl duration-300 cursor-pointer rounded ${
+                  activeSection.name === section.name ? "bg-gold/20" : ""
+                }`}
+                key={section.name}
+                onClick={() => setActiveSection(section)}
+              >
+                <h5>{section.name}</h5>
+              </div>
+            ))}
+          </div>
         </div>
-
-        <div className=" border p-3 space-y-1  border-gold/10 col-span-2">
-          {sections.map((section) => (
-            <div
-              className={`p-2 px-4 hover:bg-gold/20  border border-gold/10 shadow-xl duration-300  ${
-                activeSection.name === section.name ? "bg-gold/20 n" : ""
-              }`}
-              key={section.name}
-              onClick={() => setActiveSection(section)}
-            >
-              <h5>{section.name}</h5>
-            </div>
-          ))}
-        </div>
-        <div className="col-span-10 border  border-gold/10 p-4 prose prose-pink overflow-auto max-h-[calc(80vh)]">
-          {activeSection.content}
-        </div>
+        <div className="w-3/4 p-8 prose prose-pink overflow-auto max-h-[calc(80vh)]">{activeSection.content}</div>
       </div>
     </ModalContainer>
-  );
-};
-
-const Banking = () => {
-  return (
-    <div>
-      <Headline>Trading</Headline>
-      <p className="my-5">
-        Trading is done through Donkeys. You must have Donkeys in your balance in order to trade. You can generate these
-        with a market. You can also trade donkeys as they are fungible.
-      </p>
-
-      <h5>Banking</h5>
-      <p className="my-5">
-        Banks exist around the map. You can trade with them or deposit resources. You can even provide liquidity. If you
-        have enough money you can create your own bank and set fees.
-      </p>
-    </div>
   );
 };
