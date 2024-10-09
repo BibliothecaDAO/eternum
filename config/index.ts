03414b802b75cfac90a083b9351a281b3efed4a5
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-10-09 17:44:45 +1100

    move bank to new fn

--- config/index.ts
+++ config/index.ts
@@ -53,3 +53,9 @@ const setupConfig = process.env.VITE_PUBLIC_DEV
 export const config = new EternumConfig(setupConfig);
 
 await config.setup(account, provider);
+
+// console.log("Waiting 30 seconds before proceeding...");
+// await new Promise((resolve) => setTimeout(resolve, 30000));
+// console.log("30 seconds have passed. Continuing...");
+
+// await config.setupBank(account, provider);
