6620aaa94296f497924815f66a51ee932e26959f
Credence <hello@zerocredence.com>
2024-10-17 01:35:48 +0100

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# build and deploy season pass contracts
+printf "\n\n"
+echo "----- Building Eternum Season Pass Contract ----- "
+
+cd season_pass/contracts && scarb --release build
+cd ../scripts/deployment && npm run deploy 
+
+# update the .env.production file with the season pass and test realms contracts addresses
+VITE_SEASON_PASS_ADDRESS=$(cat ./addresses/dev/season_pass.json | jq -r '.address')
+VITE_REALMS_ADDRESS=$(cat ./addresses/dev/test_realms.json | jq -r '.address')
+
+# remove the old addresses if they exist
+ENV_FILE=../../../client/.env.production
+sed -i '' '/VITE_SEASON_PASS_ADDRESS=/d' $ENV_FILE
+sed -i '' '/VITE_REALMS_ADDRESS=/d' $ENV_FILE
+
+# add the new addresses to the ENV file
+echo "" >> $ENV_FILE
+echo "VITE_SEASON_PASS_ADDRESS=$VITE_SEASON_PASS_ADDRESS" >> $ENV_FILE
+echo "VITE_REALMS_ADDRESS=$VITE_REALMS_ADDRESS" >> $ENV_FILE
+
+cd ../../../
+printf "\n\n"
+
+
+# build eternum contracts
 cd contracts
 
 
