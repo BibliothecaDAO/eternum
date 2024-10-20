ff9b5c6aa82d017b51ce75df3d4ced91351bf362
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-10-21 10:54:51 +1100

    landing

--- landing/src/components/modules/season-pass-row.tsx
+++ landing/src/components/modules/season-pass-row.tsx
@@ -7,30 +7,32 @@ interface SeasonPassRowProps {
 
 export const SeasonPassRow = ({ seasonPasses }: SeasonPassRowProps) => {
   return (
-    <motion.div
-      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
-      initial="hidden"
-      animate="visible"
-      variants={{
-        visible: {
-          transition: {
-            staggerChildren: 0.1,
+    <>
+      <motion.div
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+        initial="hidden"
+        animate="visible"
+        variants={{
+          visible: {
+            transition: {
+              staggerChildren: 0.1,
+            },
           },
-        },
-      }}
-    >
-      {seasonPasses.map((seasonPass, index) => (
-        <motion.div
-          key={`${seasonPass.title}-${index}`}
-          variants={{
-            hidden: { opacity: 0, y: 20 },
-            visible: { opacity: 1, y: 0 },
-          }}
-          transition={{ duration: 0.5 }}
-        >
-          <SeasonPass {...seasonPass} />
-        </motion.div>
-      ))}
-    </motion.div>
+        }}
+      >
+        {seasonPasses.map((seasonPass, index) => (
+          <motion.div
+            key={`${seasonPass.title}-${index}`}
+            variants={{
+              hidden: { opacity: 0, y: 20 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            transition={{ duration: 0.5 }}
+          >
+            <SeasonPass {...seasonPass} />
+          </motion.div>
+        ))}
+      </motion.div>
+    </>
   );
 };
