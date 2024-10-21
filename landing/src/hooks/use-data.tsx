ebfba962dd87980e2b0e87af54016504f8599c7e
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-10-22 10:27:44 +1100

    remove log

--- landing/src/hooks/use-data.tsx
+++ landing/src/hooks/use-data.tsx
@@ -3,7 +3,6 @@ import { execute } from "./gql";
 import { GET_USERS } from "./query/players";
 
 export function useData() {
-  console.log(GET_USERS.toString());
   const { data } = useQuery({
     queryKey: ["number-of-players"],
     queryFn: () => execute(GET_USERS),
