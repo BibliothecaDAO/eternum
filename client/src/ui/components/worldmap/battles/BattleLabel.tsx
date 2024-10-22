611bb43b392fe30a1551e77a4db8041e2edc081b
aymericdelab <aymericdelabrousse@gmail.com>
2024-10-22 09:43:49 +0200

    fix structure siege

--- client/src/ui/components/worldmap/battles/BattleLabel.tsx
+++ client/src/ui/components/worldmap/battles/BattleLabel.tsx
@@ -2,6 +2,7 @@ import { BattleManager } from "@/dojo/modelManager/BattleManager";
 import { DojoResult, useDojo } from "@/hooks/context/DojoContext";
 import { useBattlesByPosition } from "@/hooks/helpers/battles/useBattles";
 import { useQuery } from "@/hooks/helpers/useQuery";
+import { Structure, useStructureByPosition } from "@/hooks/helpers/useStructures";
 import useUIStore from "@/hooks/store/useUIStore";
 import { BaseThreeTooltip, Position } from "@/ui/elements/BaseThreeTooltip";
 import { Headline } from "@/ui/elements/Headline";
@@ -12,12 +13,15 @@ import { useMemo } from "react";
 export const BattleInfoLabel = () => {
   const dojo = useDojo();
   const { isMapView } = useQuery();
+  const getStructure = useStructureByPosition();
   const hoveredBattlePosition = useUIStore((state) => state.hoveredBattle);
   const currentTimestamp = useUIStore.getState().nextBlockTimestamp || 0;
 
   const battles = useBattlesByPosition({ x: hoveredBattlePosition?.x || 0, y: hoveredBattlePosition?.y || 0 }).filter(
     (battle) => battle.duration_left > 0,
   );
+
+  const structure = getStructure({ x: hoveredBattlePosition?.x || 0, y: hoveredBattlePosition?.y || 0 });
 
   return (
     <>
@@ -33,6 +37,7 @@ export const BattleInfoLabel = () => {
                 battleEntityId={battle.entity_id}
                 dojo={dojo}
                 currentTimestamp={currentTimestamp}
+                structure={structure}
               />
             ))}
           </div>
@@ -46,10 +51,12 @@ const BattleInfo = ({
   battleEntityId,
   dojo,
   currentTimestamp,
+  structure,
 }: {
   battleEntityId: number;
   dojo: DojoResult;
   currentTimestamp: number;
+  structure: Structure | undefined;
 }) => {
   const battleManager = useMemo(() => new BattleManager(battleEntityId, dojo), [battleEntityId, dojo]);
 
@@ -79,7 +86,7 @@ const BattleInfo = ({
       <ProgressBar className={"w-full"} attackingHealth={attackerHealth} defendingHealth={defenderHealth} />
       <div className="text-center">
         {(isOngoing || isSiege) && (
-          <DurationLeft battleManager={battleManager} currentTimestamp={currentTimestamp} structure={undefined} />
+          <DurationLeft battleManager={battleManager} currentTimestamp={currentTimestamp} structure={structure} />
         )}
       </div>
     </div>
