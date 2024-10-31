f260a4955dde30add0c0b65978d33b30ec4ac130
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2024-10-31 17:08:46 +1100

    fix cartridge connector

--- landing/src/components/providers/Starknet.tsx
+++ landing/src/components/providers/Starknet.tsx
@@ -4,35 +4,37 @@ import ControllerConnector from "@cartridge/connector/controller";
 import { sepolia } from "@starknet-react/chains";
 import { StarknetConfig, argent, braavos, useInjectedConnectors, voyager } from "@starknet-react/core";
 import { RpcProvider } from "starknet";
-
-export function StarknetProvider({ children }: { children: React.ReactNode }) {
-  function provider(/*chain: Chain*/) {
-    return new RpcProvider({
-      nodeUrl: "https://api.cartridge.gg/x/starknet/sepolia",
-    });
-  }
-  const ETH_TOKEN_ADDRESS =
-  "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
-
-  const cartridge = new ControllerConnector({
-    policies: [
-      {
-        target: ETH_TOKEN_ADDRESS,
-        method: "approve",
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      },
-      // Add more policies as needed
-    ],
-    rpc: "https://api.cartridge.gg/x/starknet/sepolia",
-    url: "https://x.cartridge.gg/",
-    indexerUrl: "https://api.cartridge.gg/x/ls-erc/torii/graphql",
-
-    // Uncomment to use a custom theme
-    // theme: "dope-wars",
-    // colorMode: "light"
+function provider(/*chain: Chain*/) {
+  return new RpcProvider({
+    nodeUrl: "https://api.cartridge.gg/x/starknet/sepolia",
   });
-  const { connectors } = useInjectedConnectors({
+}
+
+const ETH_TOKEN_ADDRESS =
+"0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
+
+const cartridge = new ControllerConnector({
+policies: [
+  {
+    target: ETH_TOKEN_ADDRESS,
+    method: "approve",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    target: ETH_TOKEN_ADDRESS,
+    method: "transfer",
+  },
+  // Add more policies as needed
+],
+rpc: "https://api.cartridge.gg/x/starknet/sepolia",
+// Uncomment to use a custom theme
+// theme: "dope-wars",
+// colorMode: "light"
+});
+export function StarknetProvider({ children }: { children: React.ReactNode }) {
+ 
+ const { connectors } = useInjectedConnectors({
     // Show these connectors if the user has no connector installed.
     recommended: [argent(), braavos()],
     // Hide recommended connectors if the user has any connector installed.
