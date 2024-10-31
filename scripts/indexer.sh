2486827f680cd8739650944a3d0de7af9e5caf9b
Credence <hello@zerocredence.com>
2024-11-01 00:35:44 +0100

    contracts: migration: update config and client

--- scripts/indexer.sh
+++ scripts/indexer.sh
@@ -57,17 +57,13 @@ echo "----- Building World -----"
 sozo build
 
 echo "----- Migrating World -----"
-sozo migrate apply
+sozo migrate
 
 
 if [[ "$setConfig" == "true" ]]; then
-    echo "----- Auth and World Contracts: Set 0.1s ----- "
-    source scripts/env_variables.sh dev
-    ./scripts/set_writer.sh --interval 0.1  --mode dev
-
     bun --env-file=../client/.env.local ../config/index.ts
 fi
 
 echo "-----  Started indexer ----- "
 rm torii.db
-torii --world 0x320b2713e324fe3125bbc42d85ff69cb3c0908b436fa38a35746dbc45deeb11 --database torii.db --allowed-origins "*"
+torii --world 0x073bad29b5c12b09f9023e8d3a5876ea6ebd41fa26cab5035369fec4691067c2 --database torii.db --allowed-origins "*"
