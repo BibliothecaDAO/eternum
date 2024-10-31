ac1ee242574463370849ea99ccfcb26b6090ebfa
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2024-10-31 14:59:30 +1100

    add top level await plugin

--- landing/vite.config.ts
+++ landing/vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
-  plugins: [svgr(), TanStackRouterVite(), react(), wasm()],
+  plugins: [svgr(), TanStackRouterVite(), react(), wasm(),topLevelAwait()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
