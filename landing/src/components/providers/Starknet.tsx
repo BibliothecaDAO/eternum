003d70f359fb360baff9a67493d7eb55a7831451
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2024-10-31 11:40:22 +1100

    add cartridge

--- landing/src/components/providers/Starknet.tsx
+++ landing/src/components/providers/Starknet.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 
+import ControllerConnector from "@cartridge/connector/controller";
 import { devnet, mainnet, sepolia } from "@starknet-react/chains";
 import { StarknetConfig, argent, braavos, publicProvider, useInjectedConnectors, voyager } from "@starknet-react/core";
 
 export function StarknetProvider({ children }: { children: React.ReactNode }) {
+
+  const cartridge = new ControllerConnector({
+    policies: [
+      {
+        target: import.meta.env.VITE_LORDS_ADDRESS,
+        method: "approve",
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      },
+      {
+        target: import.meta.env.VITE_LORDS_ADDRESS,
+        method: "transfer",
+      },
+      // Add more policies as needed
+    ],
+    rpc: "https://api.cartridge.gg/x/starknet/sepolia",
+    // Uncomment to use a custom theme
+    // theme: "dope-wars",
+    // colorMode: "light"
+  });
   const { connectors } = useInjectedConnectors({
     // Show these connectors if the user has no connector installed.
     recommended: [argent(), braavos()],
@@ -17,7 +38,7 @@ export function StarknetProvider({ children }: { children: React.ReactNode }) {
     <StarknetConfig
       chains={[mainnet, sepolia, devnet]}
       provider={publicProvider()}
-      connectors={connectors}
+      connectors={[...connectors, cartridge]}
       explorer={voyager}
     >
       {children}
