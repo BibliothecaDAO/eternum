c40fd9d2c03f6a3d625687ac188c71f9a632a8e0
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-11-04 10:48:04 +1100

    deployment work

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-# build and deploy season pass contracts
+# Create a Slot -> if deploying to slot
+slot deployments create -t epic eternum-rc0 katana --version v1.0.0-rc.0 --invoke-max-steps 10000000 --disable-fee true --block-time 2000
+
+# get accounts 
+# -> update prod env_variables.sh
+# -> update .env.production
+# -> update dojo_prod.toml
+slot deployments accounts eternum-rc0 katana 
+
+# get variables
+VITE_PUBLIC_MASTER_ADDRESS=0x46f957b7fe3335010607174edd5c4c3fae87b12c3760dc167ac738959d8c03b
+VITE_PUBLIC_MASTER_PRIVATE_KEY=0x66184a4482615568262bb3e63eb02517eece88afc39c3b5222467e7551611ad
+VITE_PUBLIC_NODE_URL=eternum-rc0
+SOZO_WORLD=0x073bad29b5c12b09f9023e8d3a5876ea6ebd41fa26cab5035369fec4691067c2
+
+# update variables
+# if you have changed the name of the world you will need to update the address 
+sh ./update_variables.sh 0x46f957b7fe3335010607174edd5c4c3fae87b12c3760dc167ac738959d8c03b 0x66184a4482615568262bb3e63eb02517eece88afc39c3b5222467e7551611ad eternum-rc0 0x073bad29b5c12b09f9023e8d3a5876ea6ebd41fa26cab5035369fec4691067c2
+
+# ------------------------------------------------------------------------------------------------
+# Build and deploy season pass contracts
+# @dev: only run this if deploying to slot
+# ------------------------------------------------------------------------------------------------
 printf "\n\n"
 echo "----- Building Eternum Season Pass Contract ----- "
 
@@ -28,23 +50,19 @@ cd ../../../
 printf "\n\n"
 
 
-# build eternum contracts
-cd contracts
+# ------------------------------------------------------------------------------------------------
+# Build and deploy eternum contracts
+# @dev: only run this if deploying to slot
+# ------------------------------------------------------------------------------------------------
 
+cd contracts
 
 echo "Build contracts..."
 sozo --profile prod build
 
 echo "Deleting previous indexer and network..."
-slot deployments delete eternum-40 torii
-slot deployments delete eternum-40 katana
-
-# If deploying to slot
-echo "Deploying world to Realms L3..."
-slot deployments create -t epic eternum-rc0 katana --version v1.0.0-rc.0 --invoke-max-steps 10000000 --disable-fee true --block-time 2000
-
-# get accounts
-slot deployments accounts eternum-rc0 katana 
+# slot deployments delete eternum-40 torii
+# slot deployments delete eternum-40 katana
 
 echo "Migrating world..."
 sozo --profile prod migrate
