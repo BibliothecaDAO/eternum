d8531e08d3c6f2007f2778b14de05d36c0efc544
0x1337 <za6ah9t@icloud.com>
2024-11-05 20:11:07 +0700

    fix: new desert models

--- client/src/three/helpers/utils.ts
+++ client/src/three/helpers/utils.ts
@@ -1,6 +1,15 @@
+import { DRACOLoader, GLTFLoader } from "three-stdlib";
+
 export function createPausedLabel() {
   const div = document.createElement("div");
   div.classList.add("rounded-md", "bg-brown/50", "text-gold", "p-1", "-translate-x-1/2", "text-xs");
   div.textContent = `⚠️ Production paused`;
   return div;
 }
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("https://www.gstatic.com/draco/versioned/decoders/1.5.5/");
+dracoLoader.preload();
+
+export const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
