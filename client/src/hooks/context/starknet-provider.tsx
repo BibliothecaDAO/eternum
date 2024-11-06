92f6a8c7353f283a8ef59fa11de4b9e431052688
bal7hazar <bal7hazar@proton.me>
2024-11-06 17:58:01 +0100

    🐛 Fix color theme

--- client/src/hooks/context/starknet-provider.tsx
+++ client/src/hooks/context/starknet-provider.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from "react";
 
 import ControllerConnector from "@cartridge/connector/controller";
+import { ColorMode } from "@cartridge/controller";
 import { mainnet, sepolia } from "@starknet-react/chains";
 import { Connector, StarknetConfig, jsonRpcProvider, voyager } from "@starknet-react/core";
 import { policies } from "./policies";
@@ -8,6 +9,7 @@ import { policies } from "./policies";
 const theme: string = "eternum";
 const slot: string = "eternum-rc1-1";
 const namespace: string = "eternum";
+const colorMode: ColorMode = "dark";
 
 const controller = new ControllerConnector({
   rpc: import.meta.env.VITE_PUBLIC_NODE_URL,
@@ -15,6 +17,7 @@ const controller = new ControllerConnector({
   slot,
   policies,
   theme,
+  colorMode,
 });
 
 export function StarknetProvider({ children }: { children: React.ReactNode }) {
