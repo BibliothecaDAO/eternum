56767f817e4ab83a922f480e20be54b1e9037a2f
tedison <timothyedison@hotmail.com>
2024-11-07 17:14:40 -0500

    add max number of events

--- client/src/dojo/setup.ts
+++ client/src/dojo/setup.ts
@@ -16,7 +16,7 @@ export async function setup({ ...config }: DojoConfig) {
 
   // fetch all existing entities from torii
   const sync = await getSyncEntities(network.toriiClient, network.contractComponents as any, undefined, [], 10_000);
-  const eventSync = getSyncEvents(network.toriiClient, network.contractComponents.events as any, undefined, []);
+  const eventSync = getSyncEvents(network.toriiClient, network.contractComponents.events as any, undefined, [], 10_000);
 
   configManager.setDojo(components);
 
