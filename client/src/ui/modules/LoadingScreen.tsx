16d8bd841b25707cf7ad862ce01e45c51249e8b2
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-11-23 21:50:35 +1100

    loading

--- client/src/ui/modules/LoadingScreen.tsx
+++ client/src/ui/modules/LoadingScreen.tsx
@@ -1,4 +1,3 @@
-import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import "../../index.css";
 import { BuildingThumbs } from "../config";
@@ -40,26 +39,17 @@ export const LoadingScreen = () => {
     <div className="relative h-screen w-screen bg-brown">
       <img className="absolute h-screen w-screen object-cover" src="/images/cover.png" alt="" />
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl text-center bg-brown/90 rounded-xl p-10 border border-gradient bg-hex-bg min-w-96 overflow-hidden">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={currentStatement}
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -20, opacity: 0 }}
-            transition={{ duration: 0.5 }}
-            className="animatedBackground"
-          >
-            New Season is Coming soon... Agents and Lords working together world building.
-            <br />
-            <CircleButton
-              tooltipLocation="bottom"
-              image={BuildingThumbs.discord}
-              label={"Discord"}
-              size="lg"
-              onClick={() => window.open("https://discord.gg/realmsworld")}
-            />
-          </motion.div>
-        </AnimatePresence>
+        <div className="p-4 text-center">
+          New Season is Coming soon... Agents and Lords working together world building.
+        </div>
+
+        <CircleButton
+          tooltipLocation="bottom"
+          image={BuildingThumbs.discord}
+          label={"Discord"}
+          size="lg"
+          onClick={() => window.open("https://discord.gg/realmsworld")}
+        />
       </div>
     </div>
   );
