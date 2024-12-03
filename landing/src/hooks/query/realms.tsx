302d67c507ffc4d8ba979955ad35f437369878da
RedBeard <90423049+RedBeardEth@users.noreply.github.com>
2024-12-03 22:25:15 +1100

    Update realms.tsx

--- landing/src/hooks/query/realms.tsx
+++ landing/src/hooks/query/realms.tsx
@@ -23,7 +23,7 @@ export const GET_ACCOUNT_TOKENS = graphql(`
 
 export const GET_ERC_MINTS = graphql(`
   query getRealmMints {
-    tokenTransfers(accountAddress: "0x037c6B561b367a85b68668e8663041b9E2F4199c346FBda97dc0c2167F7A6016", limit: 8000) {
+    tokenTransfers(accountAddress: "0x0", limit: 8000) {
       edges {
         node {
           tokenMetadata {
