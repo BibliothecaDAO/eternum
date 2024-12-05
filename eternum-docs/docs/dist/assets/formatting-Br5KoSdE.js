2a73837a4d2d94feb7f9c87f59d9ea16b24d6f74
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-12-05 18:00:34 +1100

    docs

--- eternum-docs/docs/dist/assets/formatting-Br5KoSdE.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./index-Uy6IXIoV.js";function o(r){return r.toString().replace(/\B(?=(\d{3})+(?!\d))/g," ")}function t(r){return r/e.resources.resourcePrecision}export{t as d,o as f};
