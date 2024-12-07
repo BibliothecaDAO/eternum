995c29137de8d4c9e383f57bf276f02c0c902090
aymericdelab <aymericdelabrousse@gmail.com>
2024-12-07 23:18:00 +0100

    config hyperstructure

--- sdk/packages/eternum/src/constants/hyperstructure.ts
+++ sdk/packages/eternum/src/constants/hyperstructure.ts
@@ -8,12 +8,12 @@ export const HYPERSTRUCTURE_CREATION_COSTS: ResourceCostMinMax[] = [
 export const HYPERSTRUCTURE_CONSTRUCTION_COSTS: ResourceCostMinMax[] = [
   { resource_tier: ResourceTier.Military, min_amount: 0, max_amount: 0 },
   { resource_tier: ResourceTier.Transport, min_amount: 0, max_amount: 0 },
-  { resource_tier: ResourceTier.Food, min_amount: 50_000, max_amount: 90_000 },
-  { resource_tier: ResourceTier.Common, min_amount: 2_000, max_amount: 5_000 },
-  { resource_tier: ResourceTier.Uncommon, min_amount: 5_000, max_amount: 7_000 },
-  { resource_tier: ResourceTier.Rare, min_amount: 1_000, max_amount: 2_000 },
-  { resource_tier: ResourceTier.Unique, min_amount: 3_000, max_amount: 5_000 },
-  { resource_tier: ResourceTier.Mythic, min_amount: 15_000, max_amount: 18_000 },
+  { resource_tier: ResourceTier.Food, min_amount: 50, max_amount: 90 },
+  { resource_tier: ResourceTier.Common, min_amount: 2, max_amount: 5 },
+  { resource_tier: ResourceTier.Uncommon, min_amount: 5, max_amount: 7 },
+  { resource_tier: ResourceTier.Rare, min_amount: 1, max_amount: 2 },
+  { resource_tier: ResourceTier.Unique, min_amount: 3, max_amount: 5 },
+  { resource_tier: ResourceTier.Mythic, min_amount: 15, max_amount: 18 },
 ];
 
 export const HYPERSTRUCTURE_TOTAL_COSTS: ResourceCostMinMax[] = [
