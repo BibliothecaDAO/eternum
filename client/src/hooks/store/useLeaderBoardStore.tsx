4b88bcfe3290d3bed5e2e37e14aa8bb8c5815845
aymericdelab <aymericdelabrousse@gmail.com>
2024-12-08 18:58:58 +0100

    remove console.log

--- client/src/hooks/store/useLeaderBoardStore.tsx
+++ client/src/hooks/store/useLeaderBoardStore.tsx
@@ -28,7 +28,7 @@ export const useSubscriptionToHyperstructureEvents = () => {
     },
   } = useDojo();
 
-  const { getContributions } = useContributions({ componentName: "useSubscriptionToHyperstructureEvents" });
+  const { getContributions } = useContributions();
 
   const finishedHyperstructures = useLeaderBoardStore((state) => state.finishedHyperstructures);
   const setFinishedHyperstructures = useLeaderBoardStore((state) => state.setFinishedHyperstructures);
