7ecf1edc5db40df5b704a92dade3a0ddb0319e93
spaghetti <112407419+spaghettiOnToast@users.noreply.github.com>
2024-12-09 00:53:40 +0000

    feat(docs): Updates and polish (#2313)
    * doc updates
    
    * doc updates
    
    * docs updates
    
    * fix: remove build script, add HS immunity clarity, remove transport time calculation, smol copy updates

--- eternum-docs/docs/dist/assets/index-BJX81sjg.js
+++
@@ -1,7 +0,0 @@
-import{d as r,j as e}from"./index-Ca64bMZ6.js";const l={layout:"docs",title:"Development",description:"undefined"};function d(t){const n={a:"a",div:"div",h1:"h1",header:"header",li:"li",ul:"ul",...r(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.header,{children:e.jsxs(n.h1,{id:"development",children:["Development",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#development",children:e.jsx(n.div,{"data-autolink-icon":!0})})]})}),`
-`,e.jsxs(n.ul,{children:[`
-`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/development/getting-started",children:"Getting Started"})}),`
-`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/development/client",children:"Client"})}),`
-`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/development/contracts",children:"Contracts"})}),`
-`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/development/sdk",children:"SDK"})}),`
-`]})]})}function s(t={}){const{wrapper:n}={...r(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(d,{...t})}):d(t)}export{s as default,l as frontmatter};
