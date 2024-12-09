457aefed6dfaeae5c8d4a09ea8831080c8a441b1
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-12-10 06:42:34 +1100

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -68,7 +68,7 @@ echo "Migrating world..."
 sozo migrate --profile prod --fee eth
 
 echo "Setting up remote indexer on slot..."
-slot deployments create -t epic sepolia-rc-13 torii --version v1.0.5 --world 0x05681ee57aef37a26483a16bb873e93ed3efdd1d44f0cb66196a39573f65e7f4 --rpc https://api.cartridge.gg/x/starknet/sepolia --indexing.pending true --config ./torii-sepolia.toml
+slot deployments create -t epic sepolia-rc-14 torii --version v1.0.5 --world 0x0144a0c7d57768a655b527c8705f8ea10ab46f1ea6b5d4e7b173917df5b02813 --rpc https://api.cartridge.gg/x/starknet/sepolia --indexing.pending true --config ./torii-sepolia.toml
 
 echo "Setting up config..."
 
