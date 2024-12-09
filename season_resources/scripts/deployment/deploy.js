2784765e8727a877a6e5f8b07bb4abe79428148c
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-12-10 00:47:43 +1100

    new release

--- season_resources/scripts/deployment/deploy.js
+++ season_resources/scripts/deployment/deploy.js
@@ -1,7 +1,4 @@
-import {
-  // deployAllSeasonResourceContract,
-  grantMinterRoleToAllSeasonResourceContracts,
-} from "./libs/commands.js";
+import { deployAllSeasonResourceContract, grantMinterRoleToAllSeasonResourceContracts } from "./libs/commands.js";
 
 console.log(`   ____          _         `.red);
 console.log(`  |    \\ ___ ___| |___ _ _ `.red);
@@ -9,5 +6,5 @@ console.log(`  |  |  | -_| . | | . | | |`.red);
 console.log(`  |____/|___|  _|_|___|_  |`.red);
 console.log(`            |_|       |___|`.red);
 
-// await deployAllSeasonResourceContract();
+await deployAllSeasonResourceContract();
 await grantMinterRoleToAllSeasonResourceContracts();
