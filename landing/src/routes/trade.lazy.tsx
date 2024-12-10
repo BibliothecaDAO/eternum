a85a1110c41a00152f075f3d6b5d6e7107bcaf3e
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2024-12-10 17:27:34 +1100

    add countdown timer to bridge

--- landing/src/routes/trade.lazy.tsx
+++ landing/src/routes/trade.lazy.tsx
@@ -1,12 +1,14 @@
 import { EternumConditionsModal } from "@/components/modules/eternum-conditions-modal";
 import { SwapPanel } from "@/components/modules/swap-panel";
+import { CountdownTimer } from "@/components/ui/elements/CountdownTimer";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/trade")({
   component: () => (
-    <div className="flex justify-center items-center h-full top-0 px-2">
+    <div className="flex justify-center items-center h-full top-0 px-2 relative">
       <SwapPanel />
       <EternumConditionsModal open={true} onOpenChange={() => {}} />
+      <CountdownTimer targetDate={new Date("2024-12-11T23:00:00")} />
     </div>
   ),
 });
