a6198995e7899265b4fa1c6dd013085ec0a04797
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2024-12-12 00:53:32 +1100

    quick fix for timer

--- landing/src/components/modules/season-start-timer.tsx
+++ landing/src/components/modules/season-start-timer.tsx
@@ -10,7 +10,7 @@ export const SeasonStartTimer = () => {
 
   return (
     <div className="text-3xl text-primary font-semibold">
-      {String(hours).padStart(2, "0")}:{String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")}
+      {"<"} {String(hours).padStart(2, "0")}:{String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")}
     </div>
   );
 };
