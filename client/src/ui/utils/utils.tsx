f9b31252c4d38d23ecfbec73a42f6aa1dd84a3cd
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-12-14 21:43:09 +1100

    donk

--- client/src/ui/utils/utils.tsx
+++ client/src/ui/utils/utils.tsx
@@ -503,6 +503,7 @@ export const adjustWonderLordsCost = (cost: ResourceCost[]): ResourceCost[] => {
 
 export const calculateDonkeysNeeded = (orderWeight: number): number => {
   const configManager = ClientConfigManager.instance();
+  const donkeyCapacityGrams = configManager.getCapacityConfig(CapacityConfigCategory.Donkey);
 
-  return Math.ceil(orderWeight / configManager.getCapacityConfig(CapacityConfigCategory.Donkey));
+  return Math.ceil((orderWeight/100) / donkeyCapacityGrams);
 };
