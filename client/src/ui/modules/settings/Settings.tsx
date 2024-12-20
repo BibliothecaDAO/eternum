8e41926a9900531f44636360a9001d328c25c105
aymericdelab <aymericdelabrousse@gmail.com>
2024-12-20 17:23:39 +0100

    fix resource arrivals DDOS

--- client/src/ui/modules/settings/Settings.tsx
+++ client/src/ui/modules/settings/Settings.tsx
@@ -18,7 +18,7 @@ import Button from "@/ui/elements/Button";
 import { Checkbox } from "@/ui/elements/Checkbox";
 import { Headline } from "@/ui/elements/Headline";
 import { RangeInput } from "@/ui/elements/RangeInput";
-import { addressToNumber, displayAddress } from "@/ui/utils/utils";
+import { addressToNumber, currencyIntlFormat, displayAddress } from "@/ui/utils/utils";
 import { ContractAddress } from "@bibliothecadao/eternum";
 import { useEffect, useRef, useState } from "react";
 import { toast } from "sonner";
@@ -41,6 +41,16 @@ export const SettingsWindow = () => {
   const setEffectsLevel = useUIStore((state) => state.setEffectsLevel);
   const isSoundOn = useUIStore((state) => state.isSoundOn);
   const toggleSound = useUIStore((state) => state.toggleSound);
+
+  const getCurrentDonkeyWeightMinimum = () => {
+    return Number(localStorage.getItem("WEIGHT_MINIMUM") || 0);
+  };
+
+  const [donkeyWeightLimit, setDonkeyWeightLimit] = useState(getCurrentDonkeyWeightMinimum());
+
+  useEffect(() => {
+    localStorage.setItem("WEIGHT_MINIMUM", donkeyWeightLimit.toString());
+  }, [donkeyWeightLimit]);
 
   const { toggleFullScreen, isFullScreen } = useScreenOrientation();
   const [fullScreen, setFullScreen] = useState<boolean>(isFullScreen());
@@ -145,6 +155,18 @@ export const SettingsWindow = () => {
 
         <RangeInput value={musicLevel} fromTitle="Mute" onChange={setMusicLevel} title="Music" />
         <RangeInput value={effectsLevel} fromTitle="Mute" onChange={setEffectsLevel} title="Effects" />
+
+        <Headline>Donkey Settings</Headline>
+        <RangeInput
+          value={donkeyWeightLimit}
+          min={0}
+          max={100000}
+          fromTitle="0"
+          toTitle={currencyIntlFormat(100000)}
+          onChange={setDonkeyWeightLimit}
+          title={`Minimum Weight: ${donkeyWeightLimit} kg`}
+        />
+
         <Button onClick={() => setShowSettings(false)} variant="outline" className="text-xxs !py-1 !px-2 mr-auto">
           Done
         </Button>
