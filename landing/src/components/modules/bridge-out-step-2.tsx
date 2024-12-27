1e4c7625147508f655d62b276f1bc452895ea369
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-12-28 09:27:50 +1100

    rever

--- landing/src/components/modules/bridge-out-step-2.tsx
+++ landing/src/components/modules/bridge-out-step-2.tsx
@@ -115,7 +115,7 @@ export const BridgeOutStep2 = () => {
         newSelected.add(BigInt(donkey?.donkeyEntityId || 0));
       }
     });
-  }, [donkeyInfos, updateResourcesFromSelectedDonkeys]);
+  }, [donkeyInfos, selectedDonkeys]);
 
   const handleRefresh = () => {
     setIsRefreshing(true);
