089adc408e8b8e3951a6afd0bcfe73f845009b56
tedison <timothyedison@hotmail.com>
2024-12-27 01:21:01 +0100

    fix points

--- client/src/dojo/modelManager/LeaderboardManager.ts
+++ client/src/dojo/modelManager/LeaderboardManager.ts
@@ -136,9 +136,10 @@ export class LeaderboardManager {
           getEntityIdFromKeys([BigInt(hyperstructure.entity_id), BigInt(i + 1)]),
         );
 
-        const epochEndTimestamp = season.is_over
-          ? season.ended_at
-          : nextEpoch?.start_timestamp ?? BigInt(currentTimestamp);
+        const epochEndTimestamp =
+          season.is_over && nextEpoch === undefined
+            ? season.ended_at
+            : nextEpoch?.start_timestamp ?? BigInt(currentTimestamp);
         const epochDuration = epochEndTimestamp - epoch.start_timestamp;
 
         const nbOfCycles = Number(epochDuration) / ClientConfigManager.instance().getTick(TickIds.Default);
