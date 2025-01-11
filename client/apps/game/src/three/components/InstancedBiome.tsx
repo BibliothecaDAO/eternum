2d9c254847e3f07cae91af0ff25b1a4be4a0f681
aymericdelab <aymericdelabrousse@gmail.com>
2025-01-11 13:52:33 +0100

    prettier format

--- client/apps/game/src/three/components/InstancedBiome.tsx
+++ client/apps/game/src/three/components/InstancedBiome.tsx
@@ -44,7 +44,7 @@ export default class InstancedModel {
         tmp.userData.isInstanceModel = true;
 
         if (!enableRaycast) {
-          tmp.raycast = () => { };
+          tmp.raycast = () => {};
         }
 
         this.mixer = new AnimationMixer(gltf.scene);
