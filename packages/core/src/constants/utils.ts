157105394e0979562a02687a2712a815d9d78233
Bob <wastchenko.constantin@gmail.com>
2025-01-23 22:44:18 +0100

    Add laborId enum + utils

--- packages/core/src/constants/utils.ts
+++ packages/core/src/constants/utils.ts
@@ -1,3 +1,4 @@
+import { ResourcesIds } from ".";
 import { resources } from "./resources";
 
 export const findResourceById = (value: number) => {
@@ -8,6 +9,14 @@ export const findResourceIdByTrait = (trait: string) => {
   // @ts-ignore
   return resources.find((e) => e?.trait === trait).id;
 };
+
+export const getLaborIdFromResourceId = (resourceType: ResourcesIds) => {
+  return 255 - resourceType;
+}
+
+export const getResourceIdFromLaborId = (laborType: ResourcesIds) => {
+  return 255 - laborType;
+}
 
 export const RESOURCE_PRECISION = 1_000_000_000;
 export const RESOURCE_MULTIPLIER = 1000;
