7955ac3a22d4b93aef8a642345676e7ec9092e7d
zabanyat.eth <za6ah9t@icloud.com>
2025-02-05 17:06:50 +0700

    feat: wasm plugin

--- client/apps/eternum-mobile/vite.config.ts
+++ client/apps/eternum-mobile/vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import path, { resolve } from "path";
 import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasm()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
