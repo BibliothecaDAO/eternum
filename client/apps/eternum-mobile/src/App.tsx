76c934698d33f4ce06758a434869db9bceab54df
zabanyat.eth <za6ah9t@icloud.com>
2025-02-05 12:24:34 +0700

    feat: base project structure

---
+++ client/apps/eternum-mobile/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./shared/components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Vite + React</h1>
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
