d442f6f5df0f5331f5e2bfd04b852061f6df5eb3
zabanyat.eth <za6ah9t@icloud.com>
2025-02-21 13:37:00 +0700

    feat: mobile localhost https

--- client/apps/eternum-mobile/vite.config.ts
+++ client/apps/eternum-mobile/vite.config.ts
@@ -1,11 +1,12 @@
 import react from "@vitejs/plugin-react";
 import path, { resolve } from "path";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 import wasm from "vite-plugin-wasm";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [react(), wasm(), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
