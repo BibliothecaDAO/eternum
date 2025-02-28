10fccb8e2e9d427a5cb4758bb66193f0f2f6269c
Francesco Ceccon <francesco@ceccon.me>
2025-02-28 20:25:59 +0100

    Fix apibara build

--- apps/indexer/apibara.config.ts
+++ apps/indexer/apibara.config.ts
@@ -1,5 +1,4 @@
-import type { Plugin } from "apibara/rollup";
-import typescript from "@rollup/plugin-typescript";
+import esbuild from "rollup-plugin-esbuild";
 import { defineConfig } from "apibara/config";
 
 export default defineConfig({
@@ -8,7 +7,8 @@ export default defineConfig({
     contractAddress:
       "0x028d709c875c0ceac3dce7065bec5328186dc89fe254527084d1689910954b0a",
   },
+  exportConditions: ["node"],
   rollupConfig: {
-    plugins: [typescript() as Plugin],
+    plugins: [esbuild()],
   },
 });
