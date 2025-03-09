5a31dbcb6785522bee287c42f8565490525e3f57
Credence <hello@zerocredence.com>
2025-03-09 15:33:02 +0100

    resource bridge: update resource bridge mechanics (#2731)

--- packages/types/src/types/common.ts
+++ packages/types/src/types/common.ts
@@ -1,14 +1,14 @@
 import { ComponentValue, Entity } from "@dojoengine/recs";
 import { Account, AccountInterface } from "starknet";
 import {
-  BiomeType,
-  BuildingType,
-  CapacityConfig,
-  QuestType,
-  RealmLevels,
-  ResourcesIds,
-  ResourceTier,
-  StructureType,
+    BiomeType,
+    BuildingType,
+    CapacityConfig,
+    QuestType,
+    RealmLevels,
+    ResourcesIds,
+    ResourceTier,
+    StructureType,
 } from "../constants";
 import { ClientComponents } from "../dojo/create-client-components";
 
