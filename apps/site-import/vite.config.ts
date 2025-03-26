8172a421c33180d05af829fa7a943c2deaaf50c6
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2025-03-26 12:09:26 +1100

    update tailwind, react 19

--- apps/site-import/vite.config.ts
+++ apps/site-import/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
