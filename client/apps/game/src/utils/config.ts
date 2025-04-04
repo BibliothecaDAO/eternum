f6d525df8703e1eae71ca65958541e2edba574ef
tedison <timothyedison@hotmail.com>
2025-04-04 16:08:43 -0400

    Finish integrating eternum-loader

--- client/apps/game/src/utils/config.ts
+++ client/apps/game/src/utils/config.ts
@@ -1,3 +1,4 @@
+import { ToriiSetting } from "@/types";
 import { Chain, getConfigFromNetwork } from "@config";
 import { env } from "./../../env";
 
@@ -5,3 +6,23 @@ export const ETERNUM_CONFIG = () => {
   const config = getConfigFromNetwork(env.VITE_PUBLIC_CHAIN! as Chain);
   return config;
 };
+
+export const TORII_SETTING = async (): Promise<string> => {
+  let toriiSetting = localStorage.getItem("TORII_SETTING") as ToriiSetting;
+  if (!toriiSetting) {
+    localStorage.setItem("TORII_SETTING", DEFAULT_TORII_SETTING);
+    toriiSetting = DEFAULT_TORII_SETTING;
+  }
+
+  let toriiUrl = toriiSetting === ToriiSetting.Local ? "http://localhost:8080" : env.VITE_PUBLIC_TORII;
+
+  const aliveCheck = await fetch(toriiUrl);
+  if (!aliveCheck.ok && aliveCheck.status !== 200) {
+    localStorage.setItem("TORII_SETTING", ToriiSetting.Remote);
+    toriiUrl = env.VITE_PUBLIC_TORII;
+  }
+
+  return toriiUrl;
+};
+
+export const DEFAULT_TORII_SETTING = ToriiSetting.Local;
