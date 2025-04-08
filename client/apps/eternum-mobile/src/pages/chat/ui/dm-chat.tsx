61124723daf9061fc8cc4d745623756c7bda994e
zabanyat.eth <za6ah9t@icloud.com>
2025-04-08 16:50:52 +0700

    fix: unused files

--- client/apps/eternum-mobile/src/pages/chat/ui/dm-chat.tsx
+++
@@ -1,1 +0,0 @@
-// This file is intentionally left empty as the DMChat functionality has been integrated into DMTab
--- client/apps/eternum-mobile/src/pages/chat/ui/tabs/events-tab.tsx
+++
@@ -1,1 +0,0 @@
-// This file is intentionally left empty as the EventsTab has been replaced by RoomsTab
