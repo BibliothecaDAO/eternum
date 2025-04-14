1074a499cd046caab10197d97e0c38e909ad8301
aymericdelab <aymericdelabrousse@gmail.com>
2025-04-14 09:17:30 +0200

    remove unused loading states

--- client/apps/game/src/hooks/store/use-world-loading.ts
+++ client/apps/game/src/hooks/store/use-world-loading.ts
@@ -3,17 +3,12 @@
  * Each property indicates whether that part is currently loading data from the blockchain.
  */
 export enum LoadingStateKey {
-  Realm = "realm",
-  SpectatorRealm = "spectatorRealm",
   Market = "market",
   AllPlayerStructures = "allPlayerStructures",
   Map = "map",
   Bank = "bank",
   World = "world",
   Hyperstructure = "hyperstructure",
-  SingleKey = "singleKey",
-  Config = "config",
-  Events = "events",
   MarketHistory = "marketHistory",
   Leaderboard = "leaderboard",
 }
@@ -29,17 +24,10 @@ export interface WorldStore {
 
 export const createWorldStoreSlice = (set: any) => ({
   loadingStates: {
-    [LoadingStateKey.Realm]: false,
-    [LoadingStateKey.SpectatorRealm]: false,
     [LoadingStateKey.Market]: false,
     [LoadingStateKey.AllPlayerStructures]: false,
     [LoadingStateKey.Map]: false,
-    [LoadingStateKey.Bank]: false,
-    [LoadingStateKey.World]: false,
     [LoadingStateKey.Hyperstructure]: false,
-    [LoadingStateKey.SingleKey]: false,
-    [LoadingStateKey.Config]: false,
-    [LoadingStateKey.Events]: false,
     [LoadingStateKey.Leaderboard]: false,
     [LoadingStateKey.MarketHistory]: false,
   },
