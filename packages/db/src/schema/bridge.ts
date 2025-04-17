5e3ac9a488cdbfc9606d83d52c812ea54a72707d
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2025-04-17 11:53:04 +1000

    changes to db

--- packages/db/src/schema/bridge.ts
+++ packages/db/src/schema/bridge.ts
@@ -54,34 +54,7 @@ export const realmsBridgeEventsRelations = relations(
     }),
   }),
 );
-export const velords_burns = pgTable(
-  "dune_velords_burns",
-  {
-    //_id: text("_id"),
-    source: text("source").notNull(),
-    amount: numeric("amount").notNull(),
-    transaction_hash: text("transaction_hash").notNull(),
-    //block_time: timestamp("block_time").notNull(),
-    epoch: timestamp("epoch", {
-      mode: "date",
-      precision: 3,
-    }).notNull(),
-    epoch_total_amount: numeric("epoch_total_amount").notNull(),
-    sender_epoch_total_amount: numeric("sender_epoch_total_amount").notNull(),
-  },
-  (t) => [primaryKey({ columns: [t.amount, t.transaction_hash] })],
-);
 
-export const velords_supply = pgTable("dune_velords_supply", {
-  //_id: text("_id"),
-  old_supply: text("old_supply").notNull(),
-  new_supply: numeric("new_supply").notNull(),
-  transaction_hash: text("transaction_hash").notNull().primaryKey(),
-  block_time: timestamp("block_time", {
-    mode: "date",
-    precision: 3,
-  }).notNull(),
-});
 
 export const realmsLordsClaims = pgTable(
   "realms_lords_claims",
--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,3 +1,4 @@
 export * from "./bridge";
 export * from "./auth";
 export * from "./governance";
+export * from "./dune";
