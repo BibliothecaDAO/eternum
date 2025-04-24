f0d402af114c3e457e73f9981be00c138803deb3
zabanyat.eth <za6ah9t@icloud.com>
2025-04-24 18:46:07 +0700

    feat: local katana support

--- client/apps/eternum-mobile/src/app/dojo/context/starknet-provider.tsx
+++ client/apps/eternum-mobile/src/app/dojo/context/starknet-provider.tsx
@@ -1,18 +1,19 @@
 import { getResourceAddresses } from "@/shared/lib/addresses";
-import ControllerConnector from "@cartridge/connector/controller";
-import { mainnet, sepolia } from "@starknet-react/chains";
+import { ControllerConnector } from "@cartridge/connector";
+import { Chain, mainnet, sepolia } from "@starknet-react/chains";
 import { Connector, StarknetConfig, jsonRpcProvider, voyager } from "@starknet-react/core";
-import React, { useCallback } from "react";
+import type React from "react";
+import { useCallback } from "react";
+import { constants, shortString } from "starknet";
 import { env } from "../../../../env";
 import { policies } from "./policies";
-import { messages } from "./signing-policy";
-
-enum StarknetChainId {
-  SN_MAIN = "0x534e5f4d41494e", // encodeShortString('SN_MAIN'),
-  SN_SEPOLIA = "0x534e5f5345504f4c4941",
-}
 
 const resourceAddresses = getResourceAddresses();
+
+const KATANA_CHAIN_ID = shortString.encodeShortString("KATANA");
+const KATANA_CHAIN_NETWORK = "Katana Local";
+const KATANA_CHAIN_NAME = "katana";
+const KATANA_RPC_URL = "http://localhost:5050";
 
 const LORDS = resourceAddresses["LORDS"][1].toString();
 const otherResources = Object.entries(resourceAddresses)
@@ -24,29 +25,52 @@ const preset: string = "eternum";
 const slot: string = env.VITE_PUBLIC_SLOT;
 const namespace: string = "s1_eternum";
 
-const controller =
-  env.VITE_PUBLIC_CHAIN === "mainnet"
-    ? new ControllerConnector({
-        chains: [{ rpcUrl: env.VITE_PUBLIC_NODE_URL }],
-        defaultChainId: StarknetChainId.SN_MAIN,
-        namespace,
-        slot,
-        preset,
-        tokens: {
-          erc20: [LORDS, ...otherResources],
-        },
-      })
-    : new ControllerConnector({
-        chains: [{ rpcUrl: env.VITE_PUBLIC_NODE_URL }],
-        defaultChainId: StarknetChainId.SN_SEPOLIA,
-        namespace,
-        slot,
-        preset,
-        policies: { contracts: policies, messages },
-        tokens: {
-          erc20: [LORDS, ...otherResources],
-        },
-      });
+const isLocal = env.VITE_PUBLIC_CHAIN === "local";
+
+const nonLocalController = new ControllerConnector({
+  chains: [
+    {
+      rpcUrl: isLocal
+        ? KATANA_RPC_URL
+        : env.VITE_PUBLIC_NODE_URL !== "http://localhost:5050"
+          ? env.VITE_PUBLIC_NODE_URL
+          : "https://api.cartridge.gg/x/starknet/sepolia",
+    },
+  ],
+  defaultChainId: isLocal
+    ? KATANA_CHAIN_ID
+    : env.VITE_PUBLIC_CHAIN === "mainnet"
+      ? constants.StarknetChainId.SN_MAIN
+      : constants.StarknetChainId.SN_SEPOLIA,
+  preset,
+  policies,
+  slot,
+  namespace,
+  tokens: {
+    erc20: [LORDS, ...otherResources],
+  },
+});
+
+const katanaLocalChain = {
+  id: BigInt(KATANA_CHAIN_ID),
+  network: KATANA_CHAIN_NETWORK,
+  name: KATANA_CHAIN_NAME,
+  nativeCurrency: {
+    address: "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+
+  rpcUrls: {
+    default: {
+      http: [KATANA_RPC_URL],
+    },
+    public: {
+      http: [KATANA_RPC_URL],
+    },
+  },
+} as const satisfies Chain;
 
 export function StarknetProvider({ children }: { children: React.ReactNode }) {
   const rpc = useCallback(() => {
@@ -55,9 +79,9 @@ export function StarknetProvider({ children }: { children: React.ReactNode }) {
 
   return (
     <StarknetConfig
-      chains={[mainnet, sepolia]}
+      chains={isLocal ? [katanaLocalChain] : [mainnet, sepolia]}
       provider={jsonRpcProvider({ rpc })}
-      connectors={[controller as never as Connector]}
+      connectors={[nonLocalController as unknown as Connector]}
       explorer={voyager}
       autoConnect
     >
