59da5187e377f6b0a484414e1a495f950d244115
tedison <timothyedison@hotmail.com>
2025-05-06 18:28:25 -0400

    Remove change to namespace

--- client/apps/landing/src/components/providers/starknet-provider.tsx
+++ client/apps/landing/src/components/providers/starknet-provider.tsx
@@ -15,7 +15,7 @@ const otherResources = Object.entries(resourceAddresses)
 
 const preset: string = "eternum";
 const slot: string = env.VITE_PUBLIC_SLOT;
-const namespace: string = "s1_eternum";
+const namespace: string = "eternum";
 const chain_id =
   env.VITE_PUBLIC_CHAIN === "mainnet" ? constants.StarknetChainId.SN_MAIN : constants.StarknetChainId.SN_SEPOLIA;
 const cartridgeController = new ControllerConnector({
