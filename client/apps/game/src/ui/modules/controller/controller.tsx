ca9efa68b0b9f62c971fddf818d127a1af220b0b
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2025-05-11 11:43:23 +1000

    wallet

--- client/apps/game/src/ui/modules/controller/controller.tsx
+++ client/apps/game/src/ui/modules/controller/controller.tsx
@@ -1,9 +1,7 @@
 import { useAccountStore } from "@/hooks/store/use-account-store";
 import { useUIStore } from "@/hooks/store/use-ui-store";
-import { BuildingThumbs } from "@/ui/config";
 import Button from "@/ui/elements/button";
-import CircleButton from "@/ui/elements/circle-button";
-import { useConnect, useDisconnect } from "@starknet-react/core";
+import { useConnect } from "@starknet-react/core";
 import { useCallback, useEffect, useState } from "react";
 
 export const Controller = ({ className, iconClassName }: { className?: string; iconClassName?: string }) => {
@@ -12,8 +10,7 @@ export const Controller = ({ className, iconClassName }: { className?: string; i
   const [showDisconnectConfirm, setShowDisconnectConfirm] = useState(false);
 
   const { connect, connectors } = useConnect();
-  const { connector, account, setAccount } = useAccountStore((state) => state);
-  const { disconnect } = useDisconnect();
+  const { connector, account } = useAccountStore((state) => state);
 
   const connectWallet = () => {
     try {
@@ -30,21 +27,6 @@ export const Controller = ({ className, iconClassName }: { className?: string; i
       connectWallet();
     }
   }, [connector, account, connectWallet]);
-
-  const handleDisconnect = useCallback(() => {
-    disconnect();
-    setAccount(null);
-    setBlankOverlay(true);
-    setShowDisconnectConfirm(false);
-  }, [disconnect, setAccount, setBlankOverlay]);
-
-  const handleShowDisconnect = useCallback(() => {
-    setShowDisconnectConfirm(true);
-  }, []);
-
-  const handleCancelDisconnect = useCallback(() => {
-    setShowDisconnectConfirm(false);
-  }, []);
 
   useEffect(() => {
     if (!connector || !connector!.controller) return;
@@ -63,24 +45,23 @@ export const Controller = ({ className, iconClassName }: { className?: string; i
     }
   }, [account]);
 
+  const handleInventoryClick = useCallback(() => {
+    if (!connector?.controller) {
+      console.error("Connector not initialized");
+
+      return;
+    }
+    connector.controller.openProfile("inventory");
+  }, [connector]);
+
   return account ? (
     <>
-      {showDisconnectConfirm ? (
-        <>
-          <Button className="bg-dark-wood !pb-0" variant="danger" onClick={handleCancelDisconnect}>
-            Close
-          </Button>
-          <CircleButton label="Logout" image={BuildingThumbs.leave} size="md" onClick={handleDisconnect}></CircleButton>
-        </>
-      ) : (
-        <Button variant="default" className="!pb-0 bg-dark-wood" onClick={handleShowDisconnect}>
-          {/* <CartridgeSmall className={`w-5 md:w-4 mr-1 md:mr-1 !fill-current self-center ${iconClassName}`} /> */}
-          <div className="self-center">{userName}</div>
-        </Button>
-      )}
+      <Button variant="default" className="bg-dark-wood !pb-0 !pt-0" onClick={handleInventoryClick}>
+        {userName}
+      </Button>
     </>
   ) : (
-    <Button className="bg-dark-wood !pb-0" variant="default" onClick={handleConnect}>
+    <Button className="bg-dark-wood !pb-0 !pt-0" variant="default" onClick={handleConnect}>
       Login
     </Button>
   );
