555a68272ffdfc690d6529bd39fcad8ad79d4b9b
zabanyat.eth <za6ah9t@icloud.com>
2025-05-19 14:36:26 +0700

    fix: game chat style

--- client/apps/game/src/ui/modules/ws-chat/chat.tsx
+++ client/apps/game/src/ui/modules/ws-chat/chat.tsx
@@ -583,7 +583,9 @@ function ChatModule() {
                     d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
                   />
                 </svg>
-                <span className="truncate max-w-[150px]">
+                <span
+                  className={`truncate max-w-[150px] ${activeRoomId || (!activeRoomId && !directMessageRecipientId) ? "font-bold" : ""}`}
+                >
                   {activeRoomId
                     ? `Room: ${availableRooms.find((room) => room.id === activeRoomId)?.name || activeRoomId}`
                     : "Game Chat"}
@@ -657,7 +659,7 @@ function ChatModule() {
                     d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
                   />
                 </svg>
-                <span className="truncate max-w-[150px]">
+                <span className={`truncate max-w-[150px] ${directMessageRecipientId ? "font-bold" : ""}`}>
                   {directMessageRecipientId
                     ? `DM: ${
                         [...onlineUsers, ...offlineUsers].find((user) => user?.id === directMessageRecipientId)
