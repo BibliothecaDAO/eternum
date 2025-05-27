a6e4a8274114f19872c49150b0225bfcd4492409
Credence <hello@zerocredence.com>
2025-05-27 04:31:39 +0100

    client: update socials tab

--- client/apps/game/src/ui/constants.ts
+++ client/apps/game/src/ui/constants.ts
@@ -2,3 +2,6 @@ export const UNDEFINED_STRUCTURE_ENTITY_ID = 0;
 
 // todo: get this from the contract
 export const PRIZE_POOL_GUILDS = 300000;
+
+// Leaderboard update intervals (in milliseconds) - for unregistered shareholder points
+export const LEADERBOARD_UPDATE_INTERVAL = 3 * 60 * 1000; // 3 minute
