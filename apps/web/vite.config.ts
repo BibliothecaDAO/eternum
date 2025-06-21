053d9bee751fa33eb847c234e1e160a130a5ec02
RedBeardEth <redbeard.eth@protonmail.com>
2025-06-21 10:16:05 +1000

    update lucide chang config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -18,4 +18,11 @@ export default defineConfig({
       include: "**/*.svg?react",
     }),
   ],
+  optimizeDeps: {
+    force: false,
+    include: ["@noble/ciphers"],
+  },
+  ssr: {
+    noExternal: ["@noble/ciphers", "lossless-json"],
+  },
 });
