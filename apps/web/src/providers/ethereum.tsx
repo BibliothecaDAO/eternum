fdadde6a54d47a441d13df82d2b8754de1f9fcca
RedBeardEth <redbeard.eth@protonmail.com>
2025-07-11 07:06:52 +1000

    test build

--- apps/web/src/providers/ethereum.tsx
+++ apps/web/src/providers/ethereum.tsx
@@ -1,33 +1,33 @@
-'use client'
-import { createAppKit } from '@reown/appkit/react'
+"use client";
 
-import { WagmiProvider } from 'wagmi'
-import { arbitrum, mainnet } from '@reown/appkit/networks'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { mainnet, sepolia } from "@reown/appkit/networks";
+import { createAppKit } from "@reown/appkit/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
 
 // 0. Setup queryClient
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 // 1. Get projectId from https://cloud.reown.com
-const projectId = '3b2f679bd9ac7c6feb93d370ad0359d7'
+const projectId = "3b2f679bd9ac7c6feb93d370ad0359d7";
 
 // 2. Create a metadata object - optional
 const metadata = {
-  name: 'Realms World',
-  description: 'Connect your Ethereum wallet to Realms World',
-  url: 'https://reown.com/appkit', // origin must match your domain & subdomain
-  icons: ['https://assets.reown.com/reown-profile-pic.png']
-}
+  name: "Realms World",
+  description: "Connect your Ethereum wallet to Realms World",
+  url: "https://account.realms.world", // origin must match your domain & subdomain
+  icons: ["https://assets.reown.com/reown-profile-pic.png"],
+};
 
 // 3. Set the networks
-const networks = [mainnet, arbitrum]
+const networks = [mainnet, sepolia];
 
 // 4. Create Wagmi Adapter
 const wagmiAdapter = new WagmiAdapter({
   networks,
   projectId,
-  ssr: true
+  ssr: true,
 });
 
 // 5. Create modal
@@ -37,15 +37,14 @@ createAppKit({
   projectId,
   metadata,
   features: {
-    analytics: true // Optional - defaults to your Cloud configuration
-  }
-})
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
 
 export function AppKitProvider({ children }) {
   return (
     <WagmiProvider config={wagmiAdapter.wagmiConfig}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
-  )
+  );
 }
-    
