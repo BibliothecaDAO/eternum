a7d5dc544abeaccb951e35bdc5f528d985424a6d
aymericdelab <aymericdelabrousse@gmail.com>
2025-07-30 12:40:46 +0200

    Add new feature: Create army flow with UX improvements

--- client/apps/game/src/ui/features/world/latest-features.ts
+++ client/apps/game/src/ui/features/world/latest-features.ts
@@ -1,5 +1,11 @@
 export const latestFeatures = [
   {
+    date: "2025-07-30",
+    title: "New UX: Create army",
+    description:
+      "New flow to create army, both from hex view and from worldmap view. In worldmap, left click on one of your structure, and then right click on an adjacent hex to open the army creation window.",
+  },
+  {
     date: "2025-07-29",
     title:
       "New shortcuts have been added to the game. You can look at the current shortcuts by clicking on the keyboard icon on the top right corner of the screen.",
