73fd84f369b6e9b2d4075729452ba3c28d32f8a4
aymericdelab <aymericdelabrousse@gmail.com>
2025-08-03 17:49:13 +0200

    Refactor ArmyModel by removing updateLabelVisibility method and adding debug logs in applyLabelTransitions for better tracking of label transitions.

--- client/apps/game/src/three/utils/label-utils.ts
+++ client/apps/game/src/three/utils/label-utils.ts
@@ -720,6 +720,7 @@ export const applyLabelTransitions = (labelsMap: Map<any, any>, cameraView: Came
       // Look for the wrapper div with transition classes
       const wrapperContainer = label.element.querySelector(".transition-all.duration-700");
       if (wrapperContainer) {
+        console.log("applyLabelTransitions found wrapper for entityId:", entityId, "classes:", wrapperContainer.className);
         // Get or create a container ID for tracking timeouts
         let containerId = (wrapperContainer as HTMLElement).dataset.containerId;
         if (!containerId) {
@@ -762,6 +763,8 @@ export const applyLabelTransitions = (labelsMap: Map<any, any>, cameraView: Came
             containerId,
           );
         }
+      } else {
+        console.log("applyLabelTransitions could NOT find wrapper for entityId:", entityId, "element:", label.element);
       }
     }
   });
