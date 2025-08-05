a126a4d39f75f37fb627836e688a37a7acbfd414
zabanyat.eth <za6ah9t@icloud.com>
2025-08-05 20:33:09 +0700

    fix: blitz music

--- client/apps/game/src/hooks/helpers/use-music.ts
+++ client/apps/game/src/hooks/helpers/use-music.ts
@@ -1,4 +1,5 @@
 import { useUIStore } from "@/hooks/store/use-ui-store";
+import { getIsBlitz } from "@/ui/constants";
 import { useEffect } from "react";
 import useSound from "use-sound";
 
@@ -9,7 +10,23 @@ type Track = {
 };
 
 // Your tracks list
-export const tracks: Track[] = [{ name: "Blitz", url: "/sound/music/minstrels/blitz.flac" }];
+export const tracks: Track[] = getIsBlitz()
+  ? [{ name: "Blitz", url: "/sound/music/minstrels/blitz.flac" }]
+  : [
+      { name: "Day Break", url: "/sound/music/DayBreak.mp3" },
+      { name: "Morning Ember", url: "/sound/music/MorningEmber.mp3" },
+      { name: "Beyond The Horizon", url: "/sound/music/BeyondTheHorizon.mp3" },
+      { name: "Celestial Shores", url: "/sound/music/CelestialShores.mp3" },
+      { name: "Frostfall", url: "/sound/music/Frostfall.mp3" },
+      { name: "Nomads Ballad", url: "/sound/music/NomadsBallad.mp3" },
+      { name: "Rain Pool", url: "/sound/music/RainPool.mp3" },
+      { name: "Shadow Song", url: "/sound/music/ShadowSong.mp3" },
+      { name: "Shining Realms", url: "/sound/music/ShiningRealms.mp3" },
+      { name: "Strangers Arrival", url: "/sound/music/StrangersArrival.mp3" },
+      { name: "Twilight Harvest", url: "/sound/music/TwilightHarvest.mp3" },
+      { name: "Wanderers Chronicle", url: "/sound/music/WanderersChronicle.mp3" },
+      { name: "Happy Realm", url: "/sound/music/happy_realm.mp3" },
+    ];
 
 export const useMusicPlayer = () => {
   const isPlaying = useUIStore((state) => state.isPlaying);
@@ -39,7 +56,6 @@ export const useMusicPlayer = () => {
     onstop: () => setIsPlaying(false),
     volume: musicLevel / 100,
     onend: () => {
-      setIsPlaying(false);
       goToNextTrack();
     },
   });
