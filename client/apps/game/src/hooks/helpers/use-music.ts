2921d781065e862de73323e0fe8a7c9948272818
zabanyat.eth <za6ah9t@icloud.com>
2025-08-05 20:49:29 +0700

    fix: music

--- client/apps/game/src/hooks/helpers/use-music.ts
+++ client/apps/game/src/hooks/helpers/use-music.ts
@@ -1,6 +1,5 @@
 import { useUIStore } from "@/hooks/store/use-ui-store";
-import { getIsBlitz } from "@/ui/constants";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import useSound from "use-sound";
 
 // Define a type for your tracks
@@ -10,23 +9,23 @@ type Track = {
 };
 
 // Your tracks list
-export const tracks: Track[] = getIsBlitz()
-  ? [{ name: "Blitz", url: "/sound/music/minstrels/blitz.flac" }]
-  : [
-      { name: "Day Break", url: "/sound/music/DayBreak.mp3" },
-      { name: "Morning Ember", url: "/sound/music/MorningEmber.mp3" },
-      { name: "Beyond The Horizon", url: "/sound/music/BeyondTheHorizon.mp3" },
-      { name: "Celestial Shores", url: "/sound/music/CelestialShores.mp3" },
-      { name: "Frostfall", url: "/sound/music/Frostfall.mp3" },
-      { name: "Nomads Ballad", url: "/sound/music/NomadsBallad.mp3" },
-      { name: "Rain Pool", url: "/sound/music/RainPool.mp3" },
-      { name: "Shadow Song", url: "/sound/music/ShadowSong.mp3" },
-      { name: "Shining Realms", url: "/sound/music/ShiningRealms.mp3" },
-      { name: "Strangers Arrival", url: "/sound/music/StrangersArrival.mp3" },
-      { name: "Twilight Harvest", url: "/sound/music/TwilightHarvest.mp3" },
-      { name: "Wanderers Chronicle", url: "/sound/music/WanderersChronicle.mp3" },
-      { name: "Happy Realm", url: "/sound/music/happy_realm.mp3" },
-    ];
+const blitzTracks: Track[] = [{ name: "Blitz", url: "/sound/music/minstrels/blitz.flac" }];
+const normalTracks: Track[] = [
+  { name: "Day Break", url: "/sound/music/DayBreak.mp3" },
+  { name: "Morning Ember", url: "/sound/music/MorningEmber.mp3" },
+  { name: "Beyond The Horizon", url: "/sound/music/BeyondTheHorizon.mp3" },
+  { name: "Celestial Shores", url: "/sound/music/CelestialShores.mp3" },
+  { name: "Frostfall", url: "/sound/music/Frostfall.mp3" },
+  { name: "Nomads Ballad", url: "/sound/music/NomadsBallad.mp3" },
+  { name: "Rain Pool", url: "/sound/music/RainPool.mp3" },
+  { name: "Shadow Song", url: "/sound/music/ShadowSong.mp3" },
+  { name: "Shining Realms", url: "/sound/music/ShiningRealms.mp3" },
+  { name: "Strangers Arrival", url: "/sound/music/StrangersArrival.mp3" },
+  { name: "Twilight Harvest", url: "/sound/music/TwilightHarvest.mp3" },
+  { name: "Wanderers Chronicle", url: "/sound/music/WanderersChronicle.mp3" },
+  { name: "Happy Realm", url: "/sound/music/happy_realm.mp3" },
+];
+export const tracks: Track[] = true ? blitzTracks : normalTracks;
 
 export const useMusicPlayer = () => {
   const isPlaying = useUIStore((state) => state.isPlaying);
@@ -39,6 +38,8 @@ export const useMusicPlayer = () => {
   const setTrackIndex = useUIStore((state) => state.setTrackIndex);
   const trackName = useUIStore((state) => state.trackName);
   const setTrackName = useUIStore((state) => state.setTrackName);
+  const isBlitz = true;
+  const tracks = useMemo(() => (isBlitz ? blitzTracks : normalTracks), [isBlitz]);
 
   const goToNextTrack = () => {
     const nextIndex = (trackIndex + 1) % tracks.length;
@@ -55,8 +56,11 @@ export const useMusicPlayer = () => {
     onplay: () => setIsPlaying(true),
     onstop: () => setIsPlaying(false),
     volume: musicLevel / 100,
+    loop: tracks.length === 1,
     onend: () => {
-      goToNextTrack();
+      if (tracks.length > 1) {
+        goToNextTrack();
+      }
     },
   });
 
