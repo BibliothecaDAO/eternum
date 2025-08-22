5749da03d1d19045e0f3de9071f9202bd5d3a11a
zabanyat.eth <za6ah9t@icloud.com>
2025-08-22 20:55:09 +0700

    fix: console log

--- client/apps/game/src/three/managers/instanced-biome.tsx
+++ client/apps/game/src/three/managers/instanced-biome.tsx
@@ -26,13 +26,6 @@ export default class InstancedModel {
     let renderOrder = 0;
     gltf.scene.traverse((child: any) => {
       if (child instanceof THREE.Mesh) {
-        const isAlt = name.toLowerCase().includes("alt");
-        if (name.toLowerCase().includes("alt")) {
-          console.log(name, "alt", child.material);
-          if (child.material.emissiveIntensity > 1) {
-            //child.material.emissiveIntensity = 15;
-          }
-        }
         if (name.toLowerCase().includes("deepocean") && child.material) {
           child.material.transparent = false;
         }
