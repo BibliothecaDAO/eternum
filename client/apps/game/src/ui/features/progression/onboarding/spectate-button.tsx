22ac1e122ae305ea580799474f2c7861ac10bc78
aymericdelab <aymericdelabrousse@gmail.com>
2025-10-15 12:03:58 +0200

    fix buttons

--- client/apps/game/src/ui/features/progression/onboarding/spectate-button.tsx
+++ client/apps/game/src/ui/features/progression/onboarding/spectate-button.tsx
@@ -1,9 +1,14 @@
 import { ReactComponent as Eye } from "@/assets/icons/eye.svg";
 import Button from "@/ui/design-system/atoms/button";
 
-export const SpectateButton = ({ onClick }: { onClick: () => void }) => {
+interface SpectateButtonProps {
+  onClick: () => void;
+  className?: string;
+}
+
+export const SpectateButton = ({ onClick, className }: SpectateButtonProps) => {
   return (
-    <Button className="w-full" onClick={onClick}>
+    <Button className={`w-full${className ? " " + className : ""}`} onClick={onClick}>
       <div className="flex items-center justify-center">
         <Eye className="w-5 h-5 mr-2 fill-brown" />
         <span>Spectate</span>
