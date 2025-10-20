c1ee6c0549068f98747aefc143870b0f50181112
aymericdelab <aymericdelabrousse@gmail.com>
2025-10-20 17:45:52 +0200

    format

--- client/apps/game/src/ui/features/progression/onboarding/spectate-button.tsx
+++ client/apps/game/src/ui/features/progression/onboarding/spectate-button.tsx
@@ -13,11 +13,7 @@ export const SpectateButton = ({ onClick, className, disabled = false }: Spectat
   }
 
   return (
-    <Button
-      className={`w-full${className ? " " + className : ""}`}
-      onClick={onClick}
-      forceUppercase={false}
-    >
+    <Button className={`w-full${className ? " " + className : ""}`} onClick={onClick} forceUppercase={false}>
       <div className="flex items-center justify-center">
         <Eye className="w-5 h-5 mr-2 fill-brown" />
         <span>Spectate</span>
