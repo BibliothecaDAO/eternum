e3b9f493548968434af7946e86ab8e80a2afa0d5
djizus <4562236+djizus@users.noreply.github.com>
2026-02-06 12:39:45 +0200

    chore: delete dead landing code (old layout, unused sections, dead barrel)

--- client/apps/game/src/ui/features/landing/index.ts
+++
@@ -1,8 +0,0 @@
-export { LandingWelcome } from "./sections/welcome";
-export { LandingAccount } from "./sections/account";
-export { LandingPlayer } from "./sections/player";
-export { LandingLeaderboard } from "./sections/leaderboard";
-export { LandingMarkets } from "./sections/markets";
-export { LandingMarketDetails } from "./sections/market-details";
-export { LandingCreateMarket, LandingCreateMarketTest } from "./sections/create-market";
-export { LandingMint } from "./sections/mint";
