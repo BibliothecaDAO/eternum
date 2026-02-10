68e05c79cac5d0f434487ee9a1b0c1d0ee32af31
aymericdelab <38816784+aymericdelab@users.noreply.github.com>
2026-02-10 13:06:22 +0100

    Remove unused onboarding layout files

--- client/apps/game/src/ui/layouts/onboarding/constants.ts
+++
@@ -1,6 +0,0 @@
-import { BOOTSTRAP_LOADING_STATEMENTS } from "@/ui/layouts/bootstrap-loading/constants";
-
-/**
- * Rotating loading statements shown while the client bootstraps.
- */
-export const DEFAULT_LOADING_STATEMENTS = BOOTSTRAP_LOADING_STATEMENTS;
