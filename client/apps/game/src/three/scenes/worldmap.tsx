869ad91baf4a6d4769102f721ae4442f0fac06a4
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2026-02-17 19:30:34 +1100

    fix(game): unblock worldmap contractComponents type cast

--- client/apps/game/src/three/scenes/worldmap.tsx
+++ client/apps/game/src/three/scenes/worldmap.tsx
@@ -356,7 +356,7 @@ export default class WorldmapScene extends HexagonScene {
     const contractCoords = new Position({ x: hexCoords.col, y: hexCoords.row }).getContract();
     const components = this.dojo.components as Parameters<typeof ensureStructureSynced>[0];
     const toriiClient = this.dojo.network?.toriiClient;
-    const contractComponents = this.dojo.network?.contractComponents as
+    const contractComponents = this.dojo.network?.contractComponents as unknown as
       | Parameters<typeof ensureStructureSynced>[2]
       | undefined;
 
